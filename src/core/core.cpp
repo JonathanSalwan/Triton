09276ebb7d0e55506d6d3f52c462c5966d9e81e9
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2015-02-06 20:21:47 +0100

    Reformating and cleaning core.cpp (no significant changes)

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,81 +1,89 @@
-
-#include <csignal>
-
-#include "pin.H"
 #include "Triton.h"
 
+#include <iostream>
+
+#include <csignal>
+#include <cstdlib>
+
+#include "pin.H"
+
+using namespace std;
+
 /* Pin options: -startAnalysis */
-KNOB<std::string>  KnobStartAnalysis(KNOB_MODE_WRITEONCE, "pintool", "startAnalysis", "none", "Start/end the analysis from a scope function");
+KNOB<string> KnobStartAnalysis(KNOB_MODE_WRITEONCE, "pintool",
+			       "startAnalysis", "none",
+			       "Start/end the analysis from a scope function");
 
 /* Pin options: -detectFormatString */
-KNOB<bool>  KnobDetectFormatString(KNOB_MODE_WRITEONCE, "pintool", "detectFormatString", "0", "Enable the format string detection analysis");
+KNOB<bool> KnobDetectFormatString(KNOB_MODE_WRITEONCE, "pintool",
+				  "detectFormatString", "0",
+				  "Enable the format string detection analysis");
 
 /* flag Lock / Unlock instrumentation */
 UINT32 _analysisStatus = LOCKED;
 
-
-
 /* Trace object */
-Trace *trace = new Trace;
-
+Trace * trace = new Trace;
 
 
 /* Usage function if Pin fail to start */
 INT32 Usage()
 {
-    std::cerr << "Triton analyzer: " << std::endl << std::endl;
-    std::cerr << " -startAnalysis <function name>       Start/end the analysis from a scope function" << std::endl;
-    std::cerr << " -detectFormatString                  Enable the format string detection analysis" << std::endl;
-    return -1;
+  cout << "Triton binary analyzer usage: triton MODE [ARGS] EXEC" << endl;
+  cout << endl;
+  cout << "Available modes are:" << endl;
+  cout << " -startAnalysis <func name>  Start/end the analysis from the function scope" << endl; 
+  cout << " -detectFormatString         Enable the format string detection analysis" << endl;
+
+  return EXIT_SUCCESS;
 }
 
 
-VOID Fini(INT32, VOID *)
+/* Final callback */
+VOID Fini (INT32, VOID *)
 {
-
 //  /* Currently used to test if all going good */
-//  std::cout << std::endl << std::endl << "[DEBUG] ---------------------------" << std::endl;
-//  std::list<Tritinst *>::iterator i;
+//  cout << endl << endl << "[DEBUG] ---------------------------" << endl;
+//  list<Tritinst *>::iterator i;
 //  for(i = trace->getInstructions().begin(); i != trace->getInstructions().end(); i++){
-//    std::cout << (*i)->getAddress() << " " << (*i)->numberOfElements() << " " << (*i)->getDisassembly() << std::endl;
+//    cout << (*i)->getAddress() << " " << (*i)->numberOfElements() << " " << (*i)->getDisassembly() << endl;
 //  }
 
-  /* Delete the trace */
+  /* Clean-up the trace */
   delete trace;
-  return;
 }
 
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-  PIN_InitSymbols();
-  if(PIN_Init(argc, argv)){
-      return Usage();
-  }
+  PIN_InitSymbols ();
+  if (PIN_Init (argc, argv))
+    {
+      return Usage ();
+    }
 
   /* We first need a target function */
-  if (KnobStartAnalysis.Value().empty())
-    return Usage();
+  if (KnobStartAnalysis.Value ().empty ())
+    return Usage ();
 
   /* Enable Intel syntax */
-  PIN_SetSyntaxIntel();
+  PIN_SetSyntaxIntel ();
 
   /* Add Image callback */
-  IMG_AddInstrumentFunction(Image, NULL);
+  IMG_AddInstrumentFunction (Image, NULL);
 
   /* Add Instructions callback */
-  INS_AddInstrumentFunction(Instruction, NULL);
+  INS_AddInstrumentFunction (Instruction, NULL);
 
-  /* Add callback call after the instrumentation */
-  PIN_AddFiniFunction(Fini, NULL);
+  /* Final callback after the instrumentation */
+  PIN_AddFiniFunction (Fini, NULL);
 
   /* Catch SIGSEGV */
-  PIN_InterceptSignal(SIGSEGV, catchSignal, NULL);
+  PIN_InterceptSignal (SIGSEGV, catchSignal, NULL);
 
   /* Rock 'n roll baby */
-  PIN_StartProgram();
+  PIN_StartProgram ();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
-
-
