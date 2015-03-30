3024b3b332361a6a591ecda267eb30167de1da7a
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-03-30 17:02:42 +0200

    Rename script example

---
+++ examples/dump_symbolic_trace.py
@@ -0,0 +1,17 @@
+
+import triton
+
+if __name__ == '__main__':
+
+    # Start the symbolic analysis from the 'check' function
+    triton.startAnalysis('check')
+
+    # Dump the symbolic trace at the end of the execution
+    triton.dumpTrace(True)
+
+    # Dump some statistics at the end of the execution
+    triton.dumpStats(True)
+
+    # Run the instrumentation - Never returns
+    triton.runProgram()
+
--- examples/test.py
+++
@@ -1,9 +0,0 @@
-
-import triton
-
-if __name__ == '__main__':
-    triton.startAnalysis('check')
-    triton.dumpTrace(True)
-    triton.dumpStats(True)
-    triton.runProgram()
-
