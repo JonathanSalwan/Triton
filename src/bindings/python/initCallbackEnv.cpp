63459781da4cc4276a95e45894895154d6e78693
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-04-20 21:43:44 +0200

    Add the CALLBACK.SYSCALL_x constants

--- src/bindings/python/initCallbackEnv.cpp
+++ src/bindings/python/initCallbackEnv.cpp
@@ -8,8 +8,10 @@
 
 void initCallbackEnv(PyObject *idCallbackClassDict)
 {
-  PyDict_SetItemString(idCallbackClassDict, "AFTER",   PyInt_FromLong(CB_AFTER));
-  PyDict_SetItemString(idCallbackClassDict, "BEFORE",  PyInt_FromLong(CB_BEFORE));
-  PyDict_SetItemString(idCallbackClassDict, "FINI",    PyInt_FromLong(CB_FINI));
+  PyDict_SetItemString(idCallbackClassDict, "AFTER", PyInt_FromLong(CB_AFTER));
+  PyDict_SetItemString(idCallbackClassDict, "BEFORE", PyInt_FromLong(CB_BEFORE));
+  PyDict_SetItemString(idCallbackClassDict, "FINI", PyInt_FromLong(CB_FINI));
+  PyDict_SetItemString(idCallbackClassDict, "SYSCALL_ENTRY", PyInt_FromLong(CB_SYSCALL_ENTRY));
+  PyDict_SetItemString(idCallbackClassDict, "SYSCALL_EXIT", PyInt_FromLong(CB_SYSCALL_EXIT));
 }
 
--- src/includes/CallbackDefine.h
+++ src/includes/CallbackDefine.h
@@ -2,8 +2,10 @@
 #ifndef   __CALLBACKDEFINE_H__
 #define   __CALLBACKDEFINE_H__
 
-#define CB_BEFORE 0
-#define CB_AFTER  1
-#define CB_FINI   2
+#define CB_BEFORE           0
+#define CB_AFTER            1
+#define CB_FINI             2
+#define CB_SYSCALL_ENTRY    3
+#define CB_SYSCALL_EXIT     4
 
 #endif     /* !__CALLBACKDEFINE_H__ */
