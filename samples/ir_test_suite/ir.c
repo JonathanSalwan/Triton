8bd8da2af6c4db9a841db7eeea168b213d8a5a67
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-05-04 19:56:36 +0200

    Update sample ir

--- samples/ir_test_suite/ir.c
+++ samples/ir_test_suite/ir.c
@@ -1,14 +1,20 @@
 
-// gcc -masm=intel ./test.c -o test
+// gcc -masm=intel ./ir.c -o ir
 
 void check(void)
 {
+  short x = -1;
+
   asm("clc");
   asm("cld");
   asm("cmc");
   asm("mov eax, 32");
   asm("mov ecx, 1");
   asm("mov ebx, eax");
+  asm("mov rbx, word ptr [rsp-0x2]");
+  asm("mov rax, 2");
+  asm("mov rcx, word ptr [rsp+rax*1]");
+  asm("mov word ptr [rsp+rax*1], rcx");
   asm("add ecx, ebx");
   asm("adc eax, ecx");
   asm("inc eax");
@@ -16,8 +22,14 @@ void check(void)
   asm("sbb eax, ecx");
   asm("cmp ecx, eax");
   asm("cmp ecx, 3");
+  asm("mov al, -1");
+  asm("movsx eax, al");
+  asm("movzx ecx, al");
+  asm("movzx rdx, word ptr [rsp-0x2]");
+  asm("movsx rax, word ptr [rsp-0x2]");
 }
 
 int main(){
   check();
 }
+
