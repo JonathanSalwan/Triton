d2a3b8396db3894505b56e9fae7859a81baf2a70
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-05-15 17:14:47 +0200

    Fix typo and mem

--- src/bindings/python/tritonCallbacks.cpp
+++ src/bindings/python/tritonCallbacks.cpp
@@ -285,11 +285,11 @@ static PyObject *Triton_getRegValue(PyObject *self, PyObject *reg)
   tritonReg = PyLong_AsLong(reg);
 
   if (tritonReg >= ID_XMM0 && tritonReg <= ID_XMM15){
-    char tmp[128 + 1] = {0};
+    char tmp[32+1] = {0};
     __uint128_t value = ap.getSSERegisterValue(tritonReg);
-    uint64_t low      = value & 0xffffffffffffffff;
-    uint64_t high     = (value >> 64) & 0xffffffffffffffff;
-    sprintf(tmp, "%lx%lx", high, low);
+    uint64_t high = (value >> 64) & 0xffffffffffffffff;
+    uint64_t low = value & 0xffffffffffffffff;
+    snprintf(tmp, sizeof(tmp), "%lx%lx", high, low);
     return PyLong_FromString(tmp, nullptr, 16);
   }
   return Py_BuildValue("k", ap.getRegisterValue(tritonReg));
