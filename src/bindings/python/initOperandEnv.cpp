b212746a02a516b51738d95b73719f171a7c755b
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-05-22 13:59:31 +0200

    setContextReg and getContextReg cannot deal with MMX register

--- src/bindings/python/initOperandEnv.cpp
+++ src/bindings/python/initOperandEnv.cpp
@@ -8,6 +8,7 @@
 void initOperandEnv(PyObject *idOperandClassDict)
 {
   PyDict_SetItemString(idOperandClassDict, "IMM",   PyInt_FromLong(IRBuilderOperand::IMM));
+  PyDict_SetItemString(idOperandClassDict, "LEA",   PyInt_FromLong(IRBuilderOperand::LEA));
   PyDict_SetItemString(idOperandClassDict, "MEM_R", PyInt_FromLong(IRBuilderOperand::MEM_R));
   PyDict_SetItemString(idOperandClassDict, "MEM_W", PyInt_FromLong(IRBuilderOperand::MEM_W));
   PyDict_SetItemString(idOperandClassDict, "REG",   PyInt_FromLong(IRBuilderOperand::REG));
--- src/bindings/python/tritonPyObject.cpp
+++ src/bindings/python/tritonPyObject.cpp
@@ -183,13 +183,13 @@ PyObject *PyInstruction(IRBuilder *irb)
 PyObject *PyOperand(TritonOperand operand)
 {
   PyObject *dictOperandClass = xPyDict_New();
-  PyDict_SetItemString(dictOperandClass, "type",          PyLong_FromLong(operand.getType()));
-  PyDict_SetItemString(dictOperandClass, "value",         PyLong_FromLong(operand.getValue()));
-  PyDict_SetItemString(dictOperandClass, "size",          PyLong_FromLong(operand.getSize()));
-  PyDict_SetItemString(dictOperandClass, "displacement",  PyLong_FromLong(operand.getDisplacement()));
   PyDict_SetItemString(dictOperandClass, "baseReg",       PyLong_FromLong(operand.getBaseReg()));
+  PyDict_SetItemString(dictOperandClass, "displacement",  PyLong_FromLong(operand.getDisplacement()));
   PyDict_SetItemString(dictOperandClass, "indexReg",      PyLong_FromLong(operand.getIndexReg()));
   PyDict_SetItemString(dictOperandClass, "memoryScale",   PyLong_FromLong(operand.getMemoryScale()));
+  PyDict_SetItemString(dictOperandClass, "size",          PyLong_FromLong(operand.getSize()));
+  PyDict_SetItemString(dictOperandClass, "type",          PyLong_FromLong(operand.getType()));
+  PyDict_SetItemString(dictOperandClass, "value",         PyLong_FromLong(operand.getValue()));
 
   /* Create the Operand class */
   PyObject *operandClassName = xPyString_FromString("Operand");
