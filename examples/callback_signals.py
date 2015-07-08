28b3282e6a184628cd6de9d71b883208ba5b73b9
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-07-08 22:14:02 +0200

    Fix example

--- examples/callback_signals.py
+++ examples/callback_signals.py
@@ -6,15 +6,15 @@ from triton import *
 #  $ ./triton ./examples/callback_signals.py ./samples/others/signals
 #  Signal 11 received on thread 0.
 #  ========================== DUMP ==========================
-#  rax:    0x00000000000000                        #98 = ((_ zero_extend 32) (_ bv234 32))
+#  rax:    0x00000000000000                        ((_ zero_extend 32) (_ bv234 32))
 #  rbx:    0x00000000000000                        UNSET
-#  rcx:    0x00000000001ba4                        #83 = ((_ zero_extend 32) ((_ extract 31 0) #81))
-#  rdx:    0x0000000000000b                        #92 = ((_ sign_extend 32) ((_ extract 31 0) #34))
-#  rdi:    0x00000000001ba4                        #96 = ((_ sign_extend 32) ((_ extract 31 0) #83))
-#  rsi:    0x00000000001ba4                        #94 = ((_ sign_extend 32) ((_ extract 31 0) #90))
-#  rbp:    0x007fff097e3540                        #10 = ((_ extract 63 0) #0)
-#  rsp:    0x007fff097e3528                        #58 = (bvsub ((_ extract 63 0) #47) (_ bv8 64))
-#  rip:    0x007f3fa0735ea7                        #99 = (_ bv139911251582629 64)
+#  rcx:    0x00000000001ba4                        ((_ zero_extend 32) ((_ extract 31 0) #81))
+#  rdx:    0x0000000000000b                        ((_ sign_extend 32) ((_ extract 31 0) #34))
+#  rdi:    0x00000000001ba4                        ((_ sign_extend 32) ((_ extract 31 0) #83))
+#  rsi:    0x00000000001ba4                        ((_ sign_extend 32) ((_ extract 31 0) #90))
+#  rbp:    0x007fff097e3540                        ((_ extract 63 0) #0)
+#  rsp:    0x007fff097e3528                        (bvsub ((_ extract 63 0) #47) (_ bv8 64))
+#  rip:    0x007f3fa0735ea7                        (_ bv139911251582629 64)
 #  r8:     0x007f3fa0a94c80                        UNSET
 #  r9:     0x007f3fb671b120                        UNSET
 #  r10:    0x00000000000000                        UNSET
@@ -39,15 +39,15 @@ from triton import *
 #  xmm13:  0x00000000000000                        UNSET
 #  xmm14:  0x00000000000000                        UNSET
 #  xmm15:  0x00000000000000                        UNSET
-#  af:     0x00000000000000                        #13 = (ite (= (_ bv16 64) (bvand (_ bv16 64) (bvxor #12 (bvxor ((_ extract 63 0) #0) (_ bv16 64))))) (_ bv1 1) (_ bv0 1))
-#  cf:     0x00000000000000                        #74 = (_ bv0 1)
+#  af:     0x00000000000000                        (ite (= (_ bv16 64) (bvand (_ bv16 64) (bvxor #12 (bvxor ((_ extract 63 0) #0) (_ bv16 64))))) (_ bv1 1) (_ bv0 1))
+#  cf:     0x00000000000000                        (_ bv0 1)
 #  df:     0x00000000000000                        UNSET
 #  if:     0x00000000000001                        UNSET
-#  of:     0x00000000000000                        #75 = (_ bv0 1)
-#  pd:     0x00000000000001                        #76 = (ite (= (parity_flag ((_ extract 7 0) #73)) (_ bv0 1)) (_ bv1 1) (_ bv0 1))
-#  sf:     0x00000000000000                        #77 = (ite (= ((_ extract 31 31) #73) (_ bv1 1)) (_ bv1 1) (_ bv0 1))
+#  of:     0x00000000000000                        (_ bv0 1)
+#  pd:     0x00000000000001                        (ite (= (parity_flag ((_ extract 7 0) #73)) (_ bv0 1)) (_ bv1 1) (_ bv0 1))
+#  sf:     0x00000000000000                        (ite (= ((_ extract 31 31) #73) (_ bv1 1)) (_ bv1 1) (_ bv0 1))
 #  tf:     0x00000000000000                        UNSET
-#  zf:     0x00000000000001                        #78 = (ite (= #73 (_ bv0 32)) (_ bv1 1) (_ bv0 1))
+#  zf:     0x00000000000001                        (ite (= #73 (_ bv0 32)) (_ bv1 1) (_ bv0 1))
 
 
 
