1c4ea5e3d584a3344182d41e286ecbb9967d841a
Jonathan Salwan <jonathan.salwan@gmail.com>
2015-11-19 22:16:34 +0100

    Use boost multiprecision even for 64b

--- src/includes/TritonTypes.h
+++ src/includes/TritonTypes.h
@@ -7,23 +7,26 @@
 
 #ifndef   TRITONTYPES_H
 #define   TRITONTYPES_H
+
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/numeric/conversion/cast.hpp>
+
 #define BIT_MAX 512
 
 typedef unsigned char                       uint8;
 typedef unsigned short                      uint16;
 typedef unsigned int                        uint32;
-typedef unsigned long long                  uint64;
+typedef boost::multiprecision::uint64 _t    uint64;
 typedef boost::multiprecision::uint128_t    uint128;
+typedef boost::multiprecision::uint256_t    uint256;
 typedef boost::multiprecision::uint512_t    uint512;
 
 typedef signed char                         sint8;
 typedef signed short                        sint16;
 typedef signed int                          sint32;
-typedef signed long long                    sint64;
+typedef boost::multiprecision::int64 _t     sint64;
 typedef boost::multiprecision::int128_t     sint128;
+typedef boost::multiprecision::int256_t     sint256;
 typedef boost::multiprecision::int512_t     sint512;
-
 
 #endif     /* !TRITONTYPES_H */
