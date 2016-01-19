dcb8311f46b8522a2c07c9b3f7774e0dbff2cfc3
Jonathan Salwan <jonathan.salwan@gmail.com>
2016-01-19 12:03:41 +0100

    Add some pintool examples

---
+++ src/examples/pin/callback_image.py
@@ -0,0 +1,42 @@
+
+from triton  import *
+from pintool import *
+
+# Output
+#
+# $ ./triton ./src/examples/pin/callback_image.py ./src/samples/ir_test_suite/ir
+# ----------
+# Image path:  /dir/Triton/samples/ir_test_suite/ir
+# Image base:  0x400000L
+# Image size:  2101312
+# ----------
+# Image path:  /lib64/ld-linux-x86-64.so.2
+# Image base:  0x7fb9a14d9000L
+# Image size:  2236744
+# ----------
+# Image path:  /lib64/libc.so.6
+# Image base:  0x7fb98b739000L
+# Image size:  3766680
+
+
+def image(imagePath, imageBase, imageSize):
+    print '----------'
+    print 'Image path: ', imagePath
+    print 'Image base: ', hex(imageBase)
+    print 'Image size: ', imageSize
+    return
+
+
+if __name__ == '__main__':
+    # Set the architecture
+    setArchitecture(ARCH.X86_64)
+
+    # Start the symbolic analysis from the Entry point
+    startAnalysisFromEntry()
+
+    # Add a callback.
+    addCallback(image, CALLBACK.IMAGE_LOAD)
+
+    # Run the instrumentation - Never returns
+    runProgram()
+
