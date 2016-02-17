2ff9fc96264c385ba6e0b188663a4d8f5b66eea2
Jonathan Salwan <jonathan.salwan@gmail.com>
2016-02-17 13:43:05 +0100

    Update examples

--- src/examples/python/symbolic_emulation.py
+++ src/examples/python/symbolic_emulation.py
@@ -3,27 +3,27 @@
 ##
 ## $ ./src/examples/python/emulation.py
 ## 400000: movabs rax, 0x4142434445464748
-##         #0 = (_ bv4702394921427289928 64) ; MOVABS operation
-##         #1 = (_ bv4194314 64) ; Program Counter
+##         ref!0 = (_ bv4702394921427289928 64) ; MOVABS operation
+##         ref!1 = (_ bv4194314 64) ; Program Counter
 ##
 ## 40000a: mov rsi, 8
-##         #2 = (_ bv8 64) ; MOV operation
-##         #3 = (_ bv4194321 64) ; Program Counter
+##         ref!2 = (_ bv8 64) ; MOV operation
+##         ref!3 = (_ bv4194321 64) ; Program Counter
 ##
 ## 400011: mov rdi, 0x10000
-##         #4 = (_ bv65536 64) ; MOV operation
-##         #5 = (_ bv4194328 64) ; Program Counter
+##         ref!4 = (_ bv65536 64) ; MOV operation
+##         ref!5 = (_ bv4194328 64) ; Program Counter
 ##
 ## 400018: mov qword ptr [rdi + rsi*2 + 0x1234], rax
-##         #6 = ((_ extract 63 56) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #7 = ((_ extract 55 48) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #8 = ((_ extract 47 40) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #9 = ((_ extract 39 32) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #10 = ((_ extract 31 24) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #11 = ((_ extract 23 16) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #12 = ((_ extract 15 8) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #13 = ((_ extract 7 0) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-##         #15 = (_ bv4194336 64) ; Program Counter
+##         ref!6 = ((_ extract 63 56) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!7 = ((_ extract 55 48) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!8 = ((_ extract 47 40) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!9 = ((_ extract 39 32) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!10 = ((_ extract 31 24) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!11 = ((_ extract 23 16) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!12 = ((_ extract 15 8) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!13 = ((_ extract 7 0) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+##         ref!15 = (_ bv4194336 64) ; Program Counter
 ##
 ## Display emulated information
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -36,27 +36,27 @@
 ##
 ## Symbolic registers information
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## rdi:64 bv[63..0] #4 = (_ bv65536 64) ; MOV operation
-## rsi:64 bv[63..0] #2 = (_ bv8 64) ; MOV operation
-## rax:64 bv[63..0] #0 = (_ bv4702394921427289928 64) ; MOVABS operation
-## rip:64 bv[63..0] #15 = (_ bv4194336 64) ; Program Counter
+## rsi:64 bv[63..0] ref!2 = (_ bv8 64) ; MOV operation
+## rax:64 bv[63..0] ref!0 = (_ bv4702394921427289928 64) ; MOVABS operation
+## rip:64 bv[63..0] ref!15 = (_ bv4194336 64) ; Program Counter
+## rdi:64 bv[63..0] ref!4 = (_ bv65536 64) ; MOV operation
 ##
 ## Symbolic memory information
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## 0x11244L #13 = ((_ extract 7 0) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-## 0x11245L #12 = ((_ extract 15 8) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-## 0x11246L #11 = ((_ extract 23 16) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-## 0x11247L #10 = ((_ extract 31 24) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-## 0x11248L #9 = ((_ extract 39 32) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-## 0x11249L #8 = ((_ extract 47 40) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-## 0x1124aL #7 = ((_ extract 55 48) ((_ extract 63 0) #0)) ; byte reference - MOV operation
-## 0x1124bL #6 = ((_ extract 63 56) ((_ extract 63 0) #0)) ; byte reference - MOV operation
+## 0x11244L ref!13 = ((_ extract 7 0) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+## 0x11245L ref!12 = ((_ extract 15 8) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+## 0x11246L ref!11 = ((_ extract 23 16) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+## 0x11247L ref!10 = ((_ extract 31 24) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+## 0x11248L ref!9 = ((_ extract 39 32) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+## 0x11249L ref!8 = ((_ extract 47 40) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+## 0x1124aL ref!7 = ((_ extract 55 48) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
+## 0x1124bL ref!6 = ((_ extract 63 56) ((_ extract 63 0) ref!0)) ; byte reference - MOV operation
 ##
 ## Craft symbolic stuffs
 ## ~~~~~~~~~~~~~~~~~~~~~
-## Memory at 0x11248 : (concat ((_ extract 7 0) #6) ((_ extract 7 0) #7) ((_ extract 7 0) #8) ((_ extract 7 0) #9))
+## Memory at 0x11248 : (concat ((_ extract 7 0) ref!6) ((_ extract 7 0) ref!7) ((_ extract 7 0) ref!8) ((_ extract 7 0) ref!9))
 ## Compute memory    : 0x41424344L
-## Register AH       : ((_ extract 15 8) #0)
+## Register AH       : ((_ extract 15 8) ref!0)
 ## Compute  AH       : 0x47L
 ##
 
