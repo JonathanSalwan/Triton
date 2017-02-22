b92302bd6f5e207eb8ecb57bef29f584cb6d3118
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-02-22 18:04:42 +0100

    Add more tests

--- src/testers/test_immediate.py
+++ src/testers/test_immediate.py
@@ -7,7 +7,33 @@ import unittest
 from triton import setArchitecture, ARCH, CPUSIZE, Immediate, OPERAND
 
 
-class TestImmediate(unittest.TestCase):
+class TestImmediate8(unittest.TestCase):
+
+    """Testing the Immediate class."""
+
+    def setUp(self):
+        """Define the arch and Immediate to test."""
+        setArchitecture(ARCH.X86_64)
+        self.imm = Immediate(0x12, CPUSIZE.BYTE)
+
+    def test_bit_size(self):
+        """Check the bitsize."""
+        self.assertEqual(self.imm.getBitSize(), 8)
+
+    def test_size(self):
+        """Check the size."""
+        self.assertEqual(self.imm.getSize(), 1)
+
+    def test_value(self):
+        """Check immediate value with different size."""
+        self.assertEqual(self.imm.getValue(), 0x12)
+
+    def test_type(self):
+        """Check immadiate type."""
+        self.assertEqual(self.imm.getType(), OPERAND.IMM)
+
+
+class TestImmediate16(unittest.TestCase):
 
     """Testing the Immediate class."""
 
@@ -39,3 +65,113 @@ class TestImmediate(unittest.TestCase):
     def test_type(self):
         """Check immadiate type."""
         self.assertEqual(self.imm.getType(), OPERAND.IMM)
+
+
+class TestImmediate32(unittest.TestCase):
+
+    """Testing the Immediate class."""
+
+    def setUp(self):
+        """Define the arch and Immediate to test."""
+        setArchitecture(ARCH.X86_64)
+        self.imm = Immediate(0x12345678, CPUSIZE.DWORD)
+
+    def test_bit_size(self):
+        """Check the bitsize."""
+        self.assertEqual(self.imm.getBitSize(), 32)
+
+    def test_size(self):
+        """Check the size."""
+        self.assertEqual(self.imm.getSize(), 4)
+
+    def test_value(self):
+        """Check immediate value with different size."""
+        self.assertEqual(self.imm.getValue(), 0x12345678)
+
+    def test_type(self):
+        """Check immadiate type."""
+        self.assertEqual(self.imm.getType(), OPERAND.IMM)
+
+
+class TestImmediate64(unittest.TestCase):
+
+    """Testing the Immediate class."""
+
+    def setUp(self):
+        """Define the arch and Immediate to test."""
+        setArchitecture(ARCH.X86_64)
+        self.imm = Immediate(0x0123456789abcdef, CPUSIZE.QWORD)
+
+    def test_bit_size(self):
+        """Check the bitsize."""
+        self.assertEqual(self.imm.getBitSize(), 64)
+
+    def test_size(self):
+        """Check the size."""
+        self.assertEqual(self.imm.getSize(), 8)
+
+    def test_value(self):
+        """Check immediate value with different size."""
+        self.assertEqual(self.imm.getValue(), 0x0123456789abcdef)
+
+    def test_type(self):
+        """Check immadiate type."""
+        self.assertEqual(self.imm.getType(), OPERAND.IMM)
+
+
+class TestNegativeImmediate(unittest.TestCase):
+
+    """Testing the Immediate class."""
+
+    def setUp(self):
+        setArchitecture(ARCH.X86_64)
+
+    def test_value(self):
+        """Check immediate value with different size."""
+        self.imm = Immediate(-4, CPUSIZE.BYTE)
+        self.assertEqual(self.imm.getValue(), 0xfc)
+
+        self.imm = Immediate(-4, CPUSIZE.WORD)
+        self.assertEqual(self.imm.getValue(), 0xfffc)
+
+        self.imm = Immediate(-4, CPUSIZE.DWORD)
+        self.assertEqual(self.imm.getValue(), 0xfffffffc)
+
+        self.imm = Immediate(-4, CPUSIZE.QWORD)
+        self.assertEqual(self.imm.getValue(), 0xfffffffffffffffc)
+
+        self.imm = Immediate(0x7123456789abcdef, CPUSIZE.QWORD)
+        self.assertEqual(self.imm.getValue(), 0x7123456789abcdef)
+
+        self.imm = Immediate(0x8123456789abcdef, CPUSIZE.QWORD)
+        self.assertEqual(self.imm.getValue(), 0x8123456789abcdef)
+
+
+class TestNegativeImmediate(unittest.TestCase):
+
+    """Testing the Immediate class."""
+
+    def setUp(self):
+        setArchitecture(ARCH.X86_64)
+
+    def test_value(self):
+        """Check immediate value with different size."""
+        self.imm = Immediate(-4, CPUSIZE.BYTE)
+        self.assertEqual(self.imm.getValue(), 0xfc)
+
+        self.imm = Immediate(-4, CPUSIZE.WORD)
+        self.assertEqual(self.imm.getValue(), 0xfffc)
+
+        self.imm = Immediate(-4, CPUSIZE.DWORD)
+        self.assertEqual(self.imm.getValue(), 0xfffffffc)
+
+        self.imm = Immediate(-4, CPUSIZE.QWORD)
+        self.assertEqual(self.imm.getValue(), 0xfffffffffffffffc)
+
+        # These ones are positives
+        self.imm = Immediate(0x7123456789abcdef, CPUSIZE.QWORD)
+        self.assertEqual(self.imm.getValue(), 0x7123456789abcdef)
+
+        self.imm = Immediate(0x8123456789abcdef, CPUSIZE.QWORD)
+        self.assertEqual(self.imm.getValue(), 0x8123456789abcdef)
+
