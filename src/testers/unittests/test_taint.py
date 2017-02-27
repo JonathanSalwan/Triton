5468ac6b6bf95400b96d7d7bfe1904e7b580cba1
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-02-27 13:29:35 +0100

    Add more tainting tests

--- src/testers/unittests/test_taint.py
+++ src/testers/unittests/test_taint.py
@@ -6,10 +6,14 @@ import unittest
 
 from triton import (setArchitecture, ARCH, REG, taintRegister, processing,
                     isRegisterTainted, Instruction, taintMemory, MemoryAccess,
-                    isMemoryTainted, untaintMemory, taintAssignmentMemoryImmediate,
-                    untaintRegister, taintAssignmentMemoryMemory,
+                    isMemoryTainted, untaintMemory, untaintRegister,
+                    taintAssignmentMemoryImmediate, taintAssignmentMemoryMemory,
                     taintAssignmentMemoryRegister, taintAssignmentRegisterImmediate,
-                    taintAssignmentRegisterMemory, taintAssignmentRegisterRegister)
+                    taintAssignmentRegisterMemory, taintAssignmentRegisterRegister,
+                    taintUnionMemoryImmediate, taintUnionMemoryMemory,
+                    taintUnionMemoryRegister, taintUnionRegisterImmediate,
+                    taintUnionRegisterMemory, taintUnionRegisterRegister,
+                    getTaintedRegisters, getTaintedMemory)
 
 
 class TestTaint(unittest.TestCase):
@@ -237,4 +241,183 @@ class TestTaint(unittest.TestCase):
 
         taintAssignmentRegisterRegister(REG.RAX, REG.RBX)
         self.assertTrue(isRegisterTainted(REG.RAX))
+
+    def test_taint_union_memory_immediate(self):
+        """Check tainting union memory U immediate."""
+        setArchitecture(ARCH.X86_64)
+
+        taintMemory(MemoryAccess(0x2000, 4))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        taintUnionMemoryImmediate(MemoryAccess(0x2000, 4))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        untaintMemory(MemoryAccess(0x2000, 4))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+    def test_taint_union_memory_memory(self):
+        """Check tainting union memory U memory."""
+        setArchitecture(ARCH.X86_64)
+
+        taintMemory(MemoryAccess(0x2000, 4))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        taintUnionMemoryMemory(MemoryAccess(0x2000, 4), MemoryAccess(0x3000, 4))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x3000, 4)))
+
+        untaintMemory(MemoryAccess(0x2000, 4))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        taintUnionMemoryMemory(MemoryAccess(0x2000, 4), MemoryAccess(0x3000, 4))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x2000, 4)))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x3000, 4)))
+
+        taintMemory(MemoryAccess(0x3000, 4))
+        taintUnionMemoryMemory(MemoryAccess(0x2000, 4), MemoryAccess(0x3000, 4))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x3000, 4)))
+
+    def test_taint_union_memory_register(self):
+        """Check tainting union memory U register."""
+        setArchitecture(ARCH.X86_64)
+
+        taintMemory(MemoryAccess(0x2000, 4))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        taintUnionMemoryRegister(MemoryAccess(0x2000, 4), REG.RAX)
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+        self.assertFalse(isRegisterTainted(REG.RAX))
+
+        untaintMemory(MemoryAccess(0x2000, 4))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x2000, 4)))
+        self.assertFalse(isRegisterTainted(REG.RAX))
+
+        taintUnionMemoryRegister(MemoryAccess(0x2000, 4), REG.RAX)
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x2000, 4)))
+        self.assertFalse(isRegisterTainted(REG.RAX))
+
+        taintRegister(REG.RAX)
+        taintUnionMemoryRegister(MemoryAccess(0x2000, 4), REG.RAX)
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+        self.assertTrue(isRegisterTainted(REG.RAX))
+
+    def test_taint_union_register_immediate(self):
+        """Check tainting union register U immediate."""
+        setArchitecture(ARCH.X86_64)
+
+        self.assertFalse(isRegisterTainted(REG.RAX))
+        taintRegister(REG.RAX)
+        self.assertTrue(isRegisterTainted(REG.RAX))
+
+        taintUnionRegisterImmediate(REG.RAX)
+        self.assertTrue(isRegisterTainted(REG.RAX))
+
+        untaintRegister(REG.RAX)
+        self.assertFalse(isRegisterTainted(REG.RAX))
+        taintUnionRegisterImmediate(REG.RAX)
+        self.assertFalse(isRegisterTainted(REG.RAX))
+
+    def test_taint_union_register_memory(self):
+        """Check tainting union register U memory."""
+        setArchitecture(ARCH.X86_64)
+
+        self.assertFalse(isRegisterTainted(REG.RAX))
+        taintRegister(REG.RAX)
+        self.assertTrue(isRegisterTainted(REG.RAX))
+
+        taintUnionRegisterMemory(REG.RAX, MemoryAccess(0x2000, 4))
+        self.assertTrue(isRegisterTainted(REG.RAX))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        untaintRegister(REG.RAX)
+        self.assertFalse(isRegisterTainted(REG.RAX))
+
+        taintUnionRegisterMemory(REG.RAX, MemoryAccess(0x2000, 4))
+        self.assertFalse(isRegisterTainted(REG.RAX))
+        self.assertFalse(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        # !T U T
+        untaintRegister(REG.RAX)
+        taintMemory(MemoryAccess(0x2000, 4))
+        taintUnionRegisterMemory(REG.RAX, MemoryAccess(0x2000, 4))
+        self.assertTrue(isRegisterTainted(REG.RAX))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+        # T U T
+        taintRegister(REG.RAX)
+        taintMemory(MemoryAccess(0x2000, 4))
+        taintUnionRegisterMemory(REG.RAX, MemoryAccess(0x2000, 4))
+        self.assertTrue(isRegisterTainted(REG.RAX))
+        self.assertTrue(isMemoryTainted(MemoryAccess(0x2000, 4)))
+
+    def test_taint_union_register_register(self):
+        """Check tainting union register U register."""
+        setArchitecture(ARCH.X86_64)
+
+        self.assertFalse(isRegisterTainted(REG.RAX))
+        taintRegister(REG.RAX)
+        self.assertTrue(isRegisterTainted(REG.RAX))
+
+        taintUnionRegisterRegister(REG.RAX, REG.RBX)
+        self.assertTrue(isRegisterTainted(REG.RAX))
+        self.assertFalse(isRegisterTainted(REG.RBX))
+
+        taintRegister(REG.RBX)
+        taintUnionRegisterRegister(REG.RAX, REG.RBX)
+        self.assertTrue(isRegisterTainted(REG.RAX))
+        self.assertTrue(isRegisterTainted(REG.RBX))
+
+        untaintRegister(REG.RAX)
+        taintRegister(REG.RBX)
+        taintUnionRegisterRegister(REG.RAX, REG.RBX)
+        self.assertTrue(isRegisterTainted(REG.RAX))
+        self.assertTrue(isRegisterTainted(REG.RBX))
+
+        untaintRegister(REG.RAX)
+        untaintRegister(REG.RBX)
+        taintUnionRegisterRegister(REG.RAX, REG.RBX)
+        self.assertFalse(isRegisterTainted(REG.RAX))
+        self.assertFalse(isRegisterTainted(REG.RBX))
+
+    def test_taint_get_tainted_registers(self):
+        """Get tainted registers"""
+        setArchitecture(ARCH.X86_64)
+
+        r = getTaintedRegisters()
+        self.assertTrue(len(r) == 0)
+
+        taintRegister(REG.EAX)
+        taintRegister(REG.AX)
+        taintRegister(REG.RBX)
+        taintRegister(REG.CL)
+        taintRegister(REG.DI)
+
+        r = getTaintedRegisters()
+        self.assertTrue(REG.RAX in r)
+        self.assertTrue(REG.RBX in r)
+        self.assertTrue(REG.RCX in r)
+        self.assertTrue(REG.RDI in r)
+
+    def test_taint_get_tainted_memory(self):
+        """Get tainted memory"""
+        setArchitecture(ARCH.X86_64)
+
+        m = getTaintedMemory()
+        self.assertTrue(len(m) == 0)
+
+        taintMemory(0x1000)
+        taintMemory(0x2000)
+        taintMemory(0x3000)
+        taintMemory(MemoryAccess(0x4000, 4))
+
+        m = getTaintedMemory()
+        self.assertTrue(0x1000 in m)
+        self.assertTrue(0x2000 in m)
+        self.assertTrue(0x3000 in m)
+        self.assertTrue(0x4000 in m)
+        self.assertTrue(0x4001 in m)
+        self.assertTrue(0x4002 in m)
+        self.assertTrue(0x4003 in m)
+        self.assertFalse(0x5000 in m)
 
