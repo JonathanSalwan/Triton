3ae19cb6a46f62d19f3960c5bb9b7481a0010886
pbrunet <pbrunet@quarkslab.com>
2017-02-28 09:05:56 +0100

    Show stdout when example test fail.

--- src/testers/unittests/test_examples.py
+++ src/testers/unittests/test_examples.py
@@ -10,6 +10,7 @@ EXAMPLE_DIR = os.path.join(os.path.dirname(__file__), "..", "..", "examples", "p
 
 
 class TestExample(unittest.TestCase):
+
     """Holder to run examples as tests."""
 
 
@@ -17,9 +18,13 @@ for example in glob.iglob(os.path.join(EXAMPLE_DIR, "*.py")):
     if "small_x86-64_symbolic_emulator.py" in example:
         continue
 
+    def test_example(self, example_name=example):
+        """Run example and show stdout in case of fail."""
+        p = subprocess.Popen(["python", example_name], stdout=subprocess.PIPE)
+        out, _ = p.communicate()
+        self.assertEqual(p.returncode, 0, "\n" + out)
+
     # Define an arguments with a default value as default value is capture at
     # lambda creation so that example_name is not in the closure of the lambda
     # function.
-    setattr(TestExample, "test_" + os.path.basename(example),
-            lambda self, example_name=example: subprocess.check_call(["python", example_name], stdout=subprocess.PIPE))
-
+    setattr(TestExample, "test_" + os.path.basename(example), test_example)
