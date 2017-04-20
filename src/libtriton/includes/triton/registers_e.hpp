3ce6a602fd03ed161f64fa585ab79e462613908a
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-04-20 11:15:44 +0200

    Coding style...

--- src/libtriton/includes/triton/registers_e.hpp
+++ src/libtriton/includes/triton/registers_e.hpp
@@ -1,5 +1,14 @@
+//! \file
+/*
+**  Copyright (C) - Triton
+**
+**  This program is under the terms of the BSD License.
+*/
+
 #ifndef TRITON_REGISTERS_E_H
 #define TRITON_REGISTERS_E_H
+
+
 
 //! The Triton namespace
 namespace triton {
@@ -19,25 +28,25 @@ namespace triton {
       enum registers_e {
         ID_REG_INVALID = 0, //!< invalid = 0
 
-#define REG_SPEC(UPPER_NAME, LOWER_NAME, X86_64_UPPER, X86_64_LOWER, X86_64_PARENT, X86_UPPER, X86_LOWER, X86_PARENT, X86_AVAIL)\
+        #define REG_SPEC(UPPER_NAME, LOWER_NAME, X86_64_UPPER, X86_64_LOWER, X86_64_PARENT, X86_UPPER, X86_LOWER, X86_PARENT, X86_AVAIL) \
         ID_REG_##UPPER_NAME,
-#define REG_SPEC_NO_CAPSTONE REG_SPEC
-#include "triton/x86.spec"
+        #define REG_SPEC_NO_CAPSTONE REG_SPEC
+        #include "triton/x86.spec"
 
         /* Must be the last item */
         ID_REG_LAST_ITEM //!< must be the last item
       };
+
   /*! @} End of arch namespace */
   };
 /*! @} End of triton namespace */
 };
 
-namespace std
-{
+namespace std {
   // Define the hash function for registers_e to be use in stl containers like unordered_map
   template <>
-  class hash<triton::arch::registers_e>: public hash<uint64_t>
-  {};
-}
+  class hash<triton::arch::registers_e>: public hash<uint64_t> {
+  };
+};
 
-#endif
+#endif /* TRITON_REGISTERS_E_H */
