fa7cf5ca006c3ac38e04a54c83da97b30c7a1a5d
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-05-30 15:57:57 +0200

    Typo

--- src/libtriton/arch/irBuilder.cpp
+++ src/libtriton/arch/irBuilder.cpp
@@ -168,7 +168,7 @@ namespace triton {
         for (auto it = inst.operands.begin(); it!= inst.operands.end(); it++) {
           if (it->getType() == triton::arch::OP_MEM) {
             if (it->getMemory().getLeaAst()->isSymbolized() == false) {
-              this->astGarbageCollector.extractUniqueAstNodes(uniqueNodes, it->getMemory().getLeaAst());
+              this->astGarbageCollector->extractUniqueAstNodes(uniqueNodes, it->getMemory().getLeaAst());
               it->getMemory().setLeaAst(nullptr);
             }
           }
