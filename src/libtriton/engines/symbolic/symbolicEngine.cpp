b1308323d4e54462f2a868870ac3b45bda969e5c
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-05-30 11:51:16 +0200

    Fix #551: ALIGNED_MEMORY optimization and symbolic memory variable

--- src/libtriton/engines/symbolic/symbolicEngine.cpp
+++ src/libtriton/engines/symbolic/symbolicEngine.cpp
@@ -592,10 +592,16 @@ namespace triton {
 
         /* First we create a symbolic variable */
         symVar = this->newSymbolicVariable(triton::engines::symbolic::MEM, memAddr, symVarSize * BYTE_SIZE_BIT, symVarComment);
+
         /* Setup the concrete value to the symbolic variable */
         symVar->setConcreteValue(cv);
+
         /* Create the AST node */
         triton::ast::AbstractNode* symVarNode = triton::ast::variable(*symVar);
+
+        /* Record the aligned symbolic variable for a symbolic optimization */
+        if (this->modes->isModeEnabled(triton::modes::ALIGNED_MEMORY))
+          this->addAlignedMemory(memAddr, symVarSize, symVarNode);
 
         /*  Split expression in bytes */
         for (triton::sint32 index = symVarSize-1; index >= 0; index--) {
@@ -619,8 +625,6 @@ namespace triton {
 
           /* Add the new memory reference */
           this->addMemoryReference(memAddr+index, se->getId());
-          if (this->modes->isModeEnabled(triton::modes::ALIGNED_MEMORY))
-            removeAlignedMemory(memAddr+index, BYTE_SIZE);
         }
 
         return symVar;
