551349243cdf5bfabdfa900f649580977537f9ef
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-07-05 22:24:25 +0200

    [python] REG.X86{_64}.X as Register class

--- src/libtriton/bindings/python/namespaces/initRegNamespace.cpp
+++ src/libtriton/bindings/python/namespaces/initRegNamespace.cpp
@@ -9,6 +9,8 @@
 #include <triton/pythonObjects.hpp>
 #include <triton/pythonUtils.hpp>
 #include <triton/pythonXFunctions.hpp>
+#include <triton/architecture.hpp>
+#include <triton/coreUtils.hpp>
 
 
 
@@ -36,7 +38,6 @@ According to the CPU architecture, the REG namespace contains all kinds of regis
 */
 
 
-
 namespace triton {
   namespace bindings {
     namespace python {
@@ -44,28 +45,39 @@ namespace triton {
       void initRegNamespace(PyObject* registersDict) {
         PyDict_Clear(registersDict);
 
+        triton::arch::Architecture x86;
+        triton::arch::Architecture x8664;
+
+        x86.setArchitecture(triton::arch::ARCH_X86);
+        x8664.setArchitecture(triton::arch::ARCH_X86_64);
+
         PyObject* x86RegistersDict      = xPyDict_New();
         PyObject* x86RegistersDictClass = xPyClass_New(nullptr, x86RegistersDict, xPyString_FromString("X86"));
         PyDict_SetItemString(registersDict, "X86", x86RegistersDictClass);
 
         // Init X86 REG namespace
-        #define REG_SPEC(UPPER_NAME, LOWER_NAME, X86_64_UPPER, X86_64_LOWER, X86_64_PARENT, X86_UPPER, X86_LOWER, X86_PARENT, X86_AVAIL)  \
-          if (X86_AVAIL)                                                                                                                  \
-            PyDict_SetItemString(x86RegistersDict, #UPPER_NAME, PyLong_FromUint32(triton::arch::ID_REG_##UPPER_NAME));
-        // Use REG not available in capstone as normal register
-        #define REG_SPEC_NO_CAPSTONE REG_SPEC
-        #include "triton/x86.spec"
+        for (const auto& reg : x86.getAllRegisters()) {
+          /* Duplicate register name */
+          std::string n(reg.second.getName());
+          /* To upper... */
+          triton::utils::toupper(n);
+          /* Add the to Python dict */
+          PyDict_SetItemString(x86RegistersDict, n.c_str(), PyRegister(reg.second));
+        }
 
         PyObject* x8664RegistersDict      = xPyDict_New();
         PyObject* x8664RegistersDictClass = xPyClass_New(nullptr, x8664RegistersDict, xPyString_FromString("X86_64"));
         PyDict_SetItemString(registersDict, "X86_64", x8664RegistersDictClass);
 
         // Init X86_64 REG namespace
-        #define REG_SPEC(UPPER_NAME, LOWER_NAME, X86_64_UPPER, X86_64_LOWER, X86_64_PARENT, X86_UPPER, X86_LOWER, X86_PARENT, X86_AVAIL)  \
-          PyDict_SetItemString(x8664RegistersDict, #UPPER_NAME, PyLong_FromUint32(triton::arch::ID_REG_##UPPER_NAME));
-        // Use REG not available in capstone as normal register
-        #define REG_SPEC_NO_CAPSTONE REG_SPEC
-        #include "triton/x86.spec"
+        for (const auto& reg : x8664.getAllRegisters()) {
+          /* Duplicate register name */
+          std::string n(reg.second.getName());
+          /* To upper... */
+          triton::utils::toupper(n);
+          /* Add the to Python dict */
+          PyDict_SetItemString(x8664RegistersDict, n.c_str(), PyRegister(reg.second));
+        }
       }
 
     }; /* python namespace */
