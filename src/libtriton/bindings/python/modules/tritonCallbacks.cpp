659eb3fc20e6c50b71b8c78deae058dcdfa2dde1
Taejoon Byun <taejoon@umn.edu>
2017-08-23 15:31:51 -0500

    fixed a bug introduced during rebasing which caused build failure

--- src/libtriton/bindings/python/modules/tritonCallbacks.cpp
+++ src/libtriton/bindings/python/modules/tritonCallbacks.cpp
@@ -142,22 +142,6 @@ namespace triton {
         }
       }
 
-      static PyObject* triton_TaintTag(PyObject* self, PyObject* args) {
-        PyObject* pDict = nullptr;
-
-        /* Extract arguments */
-        if (!PyArg_ParseTuple(args, "O!", &PyDict_Type, &pDict)) {
-          return PyErr_Format(PyExc_TypeError, \
-              "TaintTag(): Expects a dictionary as the first argument.");
-        }
-
-        try {
-          triton::engines::taint::TaintTag tag(pDict);
-          return PyTaintTag(tag);
-        } catch (const triton::exceptions::Exception& e) {
-          return PyErr_Format(PyExc_TypeError, "%s", e.what());
-        }
-      }
 
       static PyObject* triton_TaintTag(PyObject* self, PyObject* args) {
         PyObject* pDict = nullptr;
