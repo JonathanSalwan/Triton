107c5194b80c3f9702caacb92a0100131ff805eb
Jonathan Salwan <jonathan.salwan@gmail.com>
2017-12-04 10:30:40 +0100

    Fix typo

--- src/libtriton/ast/astContext.cpp
+++ src/libtriton/ast/astContext.cpp
@@ -487,7 +487,7 @@ namespace triton {
     void AstContext::updateVariable(const std::string& name, const triton::uint512& value) {
       for (auto& kv: this->astGarbageCollector.getAstVariableNodes()) {
         if (kv.first == name) {
-          assert(kv.second[0]->getType() == triton::ast::VARIABLE_NODE);
+          assert(kv.second[0]->getKind() == triton::ast::VARIABLE_NODE);
           this->valueMapping[dynamic_cast<VariableNode*>(kv.second[0])->getVar().getName()] = value;
           for (auto* N: kv.second)
             N->init();
