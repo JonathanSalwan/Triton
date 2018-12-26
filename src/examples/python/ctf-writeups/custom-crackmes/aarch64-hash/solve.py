f2a5108b03a1633323c8ecba0984ce5eb38416b1
Jonathan Salwan <jonathan.salwan@gmail.com>
2018-12-26 15:54:32 +0100

    Add a new example to solve an aarch64 chall

--- src/examples/python/ctf-writeups/custom-crackmes/aarch64-hash/solve.py
+++ src/examples/python/ctf-writeups/custom-crackmes/aarch64-hash/solve.py
@@ -3,10 +3,67 @@
 ##
 ##  Jonathan Salwan - 2018-12-26
 ##
-##  A custom crackme to test the AArch64 architecture.
+##  A custom crackme to test the AArch64 architecture. The goal is to find an
+##  hash collision to take the 'Win' branch. Firs we run the binary with a random
+##  seed, then we calculate the hash collision and run a second time the binary with
+##  the good input to take the 'Win' branch.
 ##
 ##  Output:
 ##
+##  $ time ./solve.py
+##  [+] Loading 0x000040 - 0x000238
+##  [+] Loading 0x000238 - 0x000253
+##  [+] Loading 0x000000 - 0x000a3c
+##  [+] Loading 0x010db8 - 0x011040
+##  [+] Loading 0x010dc8 - 0x010fa8
+##  [+] Loading 0x000254 - 0x000274
+##  [+] Loading 0x000948 - 0x000984
+##  [+] Loading 0x000000 - 0x000000
+##  [+] Loading 0x010db8 - 0x011000
+##  [+] Hooking __libc_start_main
+##  [+] Hooking puts
+##  [+] Starting emulation.
+##  [+] __libc_start_main hooked
+##  [+] argv[0] = ./crackme_hash
+##  [+] argv[1] = arm64
+##  [+] Please wait, calculating hash collisions...
+##  [+] Found several hash collisions:
+##  {0L: "0x6c, 'l'", 1L: "0x78, 'x'", 2L: "0x75, 'u'", 3L: "0x70, 'p'", 4L: "0x6e, 'n'"}
+##  {0L: "0x63, 'c'", 1L: "0x78, 'x'", 2L: "0x62, 'b'", 3L: "0x70, 'p'", 4L: "0x62, 'b'"}
+##  {0L: "0x73, 's'", 1L: "0x68, 'h'", 2L: "0x62, 'b'", 3L: "0x70, 'p'", 4L: "0x62, 'b'"}
+##  {0L: "0x71, 'q'", 1L: "0x66, 'f'", 2L: "0x62, 'b'", 3L: "0x70, 'p'", 4L: "0x62, 'b'"}
+##  {0L: "0x75, 'u'", 1L: "0x66, 'f'", 2L: "0x66, 'f'", 3L: "0x70, 'p'", 4L: "0x62, 'b'"}
+##  {0L: "0x75, 'u'", 1L: "0x67, 'g'", 2L: "0x67, 'g'", 3L: "0x70, 'p'", 4L: "0x62, 'b'"}
+##  {0L: "0x75, 'u'", 1L: "0x6f, 'o'", 2L: "0x67, 'g'", 3L: "0x78, 'x'", 4L: "0x62, 'b'"}
+##  {0L: "0x75, 'u'", 1L: "0x6f, 'o'", 2L: "0x67, 'g'", 3L: "0x70, 'p'", 4L: "0x6a, 'j'"}
+##  {0L: "0x75, 'u'", 1L: "0x6f, 'o'", 2L: "0x67, 'g'", 3L: "0x74, 't'", 4L: "0x6e, 'n'"}
+##  {0L: "0x75, 'u'", 1L: "0x6f, 'o'", 2L: "0x67, 'g'", 3L: "0x75, 'u'", 4L: "0x6f, 'o'"}
+##  {0L: "0x76, 'v'", 1L: "0x70, 'p'", 2L: "0x67, 'g'", 3L: "0x75, 'u'", 4L: "0x6f, 'o'"}
+##  {0L: "0x77, 'w'", 1L: "0x70, 'p'", 2L: "0x66, 'f'", 3L: "0x75, 'u'", 4L: "0x6f, 'o'"}
+##  {0L: "0x77, 'w'", 1L: "0x70, 'p'", 2L: "0x66, 'f'", 3L: "0x71, 'q'", 4L: "0x6b, 'k'"}
+##  {0L: "0x76, 'v'", 1L: "0x70, 'p'", 2L: "0x67, 'g'", 3L: "0x71, 'q'", 4L: "0x6b, 'k'"}
+##  {0L: "0x76, 'v'", 1L: "0x70, 'p'", 2L: "0x67, 'g'", 3L: "0x70, 'p'", 4L: "0x6a, 'j'"}
+##  {0L: "0x77, 'w'", 1L: "0x70, 'p'", 2L: "0x66, 'f'", 3L: "0x70, 'p'", 4L: "0x6a, 'j'"}
+##  {0L: "0x77, 'w'", 1L: "0x70, 'p'", 2L: "0x66, 'f'", 3L: "0x72, 'r'", 4L: "0x6c, 'l'"}
+##  {0L: "0x77, 'w'", 1L: "0x6e, 'n'", 2L: "0x64, 'd'", 3L: "0x72, 'r'", 4L: "0x6c, 'l'"}
+##  {0L: "0x75, 'u'", 1L: "0x6c, 'l'", 2L: "0x64, 'd'", 3L: "0x72, 'r'", 4L: "0x6c, 'l'"}
+##  {0L: "0x75, 'u'", 1L: "0x6e, 'n'", 2L: "0x66, 'f'", 3L: "0x72, 'r'", 4L: "0x6c, 'l'"}
+##  [+] Pick up the first serial: lxupn
+##  [+] puts hooked
+##  fail
+##  [+] Instruction executed: 240
+##  [+] Emulation done.
+##  [+] Start a second emualtion with the good serial to validate the chall
+##  [+] Starting emulation.
+##  [+] __libc_start_main hooked
+##  [+] argv[0] = ./crackme_hash
+##  [+] argv[1] = lxupn
+##  [+] puts hooked
+##  Win
+##  [+] Instruction executed: 240
+##  [+] Emulation done.
+##
+##  ./solve.py  0.10s user 0.00s system 99% cpu 0.105 total
 ##
 
 import random
@@ -17,8 +74,11 @@ import os
 
 from triton import *
 
-TARGET = os.path.join(os.path.dirname(__file__), 'crackme_hash')
 DEBUG  = True
+INPUT  = 'arm64'
+SERIAL = None
+TARGET = os.path.join(os.path.dirname(__file__), 'crackme_hash')
+VALID  = False
 
 # The debug function
 def debug(s):
@@ -80,7 +140,7 @@ def libcMainHandler(ctx):
 
     argvs = [
         TARGET, # argv[0]
-        "love aarch64"
+        INPUT
     ]
 
     # Define argc / argv
@@ -91,9 +151,11 @@ def libcMainHandler(ctx):
     for argv in argvs:
         addrs.append(base)
         ctx.setConcreteMemoryAreaValue(base, argv+'\x00')
-        for indexCell in range(len(argv)):
-            var = ctx.convertMemoryToSymbolicVariable(MemoryAccess(base+indexCell, CPUSIZE.BYTE))
-            var.setComment('argv[%d][%d]' %(index, indexCell))
+        if index == 1:
+            # Only symbolized argv[1]
+            for indexCell in range(len(argv)):
+                var = ctx.convertMemoryToSymbolicVariable(MemoryAccess(base+indexCell, CPUSIZE.BYTE))
+                var.setComment('argv[%d][%d]' %(index, indexCell))
         debug('[+] argv[%d] = %s' %(index, argv))
         base += len(argv)+1
         index += 1
@@ -143,6 +205,9 @@ def hookingHandler(ctx):
 
 # Emulate the binary.
 def emulate(ctx, pc):
+    global SERIAL
+    global VALID
+
     count = 0
     while pc:
         # Fetch opcodes
@@ -158,14 +223,59 @@ def emulate(ctx, pc):
             debug('[-] Instruction not supported: %s' %(str(instruction)))
             break
 
+        #print instruction
+
+        # .text:0000000000000864 ADRP  X0, #aWin@PAGE ; "Win"
+        # .text:0000000000000868 ADD   X0, X0, #aWin@PAGEOFF ; "Win"
+        # .text:000000000000086C BL    .puts
+        if pc == 0x868:
+            # We validated the crackme
+            VALID = True
+
+        # .text:0000000000000858 MOV   W0, #0xAD6D
+        # .text:000000000000085C CMP   W1, W0
+        # .text:0000000000000860 B.NE  loc_874
+        if pc == 0x85c and SERIAL is None:
+            print '[+] Please wait, calculating hash collisions...'
+            x1 = ctx.getSymbolicRegister(ctx.registers.x1)
+
+            SymVar_0 = ctx.getSymbolicVariableFromName('SymVar_0')
+            SymVar_1 = ctx.getSymbolicVariableFromName('SymVar_1')
+            SymVar_2 = ctx.getSymbolicVariableFromName('SymVar_2')
+            SymVar_3 = ctx.getSymbolicVariableFromName('SymVar_3')
+            SymVar_4 = ctx.getSymbolicVariableFromName('SymVar_4')
+
+            astCtxt = ctx.getAstContext()
+
+            # We want printable characters
+            expr = astCtxt.land([
+                     astCtxt.bvugt(astCtxt.variable(SymVar_0), astCtxt.bv(96,  CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvult(astCtxt.variable(SymVar_0), astCtxt.bv(123, CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvugt(astCtxt.variable(SymVar_1), astCtxt.bv(96,  CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvult(astCtxt.variable(SymVar_1), astCtxt.bv(123, CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvugt(astCtxt.variable(SymVar_2), astCtxt.bv(96,  CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvult(astCtxt.variable(SymVar_2), astCtxt.bv(123, CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvugt(astCtxt.variable(SymVar_3), astCtxt.bv(96,  CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvult(astCtxt.variable(SymVar_3), astCtxt.bv(123, CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvugt(astCtxt.variable(SymVar_4), astCtxt.bv(96,  CPUSIZE.BYTE_BIT)),
+                     astCtxt.bvult(astCtxt.variable(SymVar_4), astCtxt.bv(123, CPUSIZE.BYTE_BIT)),
+                     astCtxt.equal(x1.getAst(), astCtxt.bv(0xad6d, CPUSIZE.QWORD_BIT)) # collision: (assert (= x1 0xad6d)
+                   ])
+
+            # Get max 20 different models
+            models = ctx.getModels(expr, 20)
+            print '[+] Found several hash collisions:'
+            for model in models:
+                print {k: "0x%x, '%c'" % (v.getValue(), v.getValue()) for k, v in model.items()}
+
+            SERIAL = str()
+            for _, v in models[0].items():
+                SERIAL += "%c" % (v.getValue())
+
+            print '[+] Pick up the first serial: %s' %(SERIAL)
+
+        # Inc the number of instructions exected
         count += 1
-
-        print instruction
-        #for x in instruction.getSymbolicExpressions():
-        #    print x
-
-        if instruction.getType() == OPCODE.AARCH64.HLT:
-            break
 
         # Simulate routines
         hookingHandler(ctx)
@@ -231,6 +341,9 @@ def run(ctx, binary):
 
 
 def main():
+    global INPUT
+    global SERIAL
+
     # Get a Triton context
     ctx = TritonContext()
 
@@ -250,9 +363,17 @@ def main():
     # Perform our own relocations
     makeRelocation(ctx, binary)
 
-    # Init and emulate
+    # First emulation
     run(ctx, binary)
-    return 0
+
+    # Replace the input with the good serial to validate the chall
+    INPUT = SERIAL
+
+    # Second emulation
+    print '[+] Start a second emualtion with the good serial to validate the chall'
+    run(ctx, binary)
+
+    return not VALID == True
 
 
 if __name__ == '__main__':
