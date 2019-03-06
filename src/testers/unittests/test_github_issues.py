026e0269371b0aac019726a9f9d31debec35c70c
Jonathan Salwan <jonathan.salwan@gmail.com>
2019-03-06 14:56:14 +0100

    Fix #656: Add unittest

--- src/testers/unittests/test_github_issues.py
+++ src/testers/unittests/test_github_issues.py
@@ -7,6 +7,47 @@ import unittest
 from triton import *
 
 
+
+class TestIssue656(unittest.TestCase):
+
+    """Testing #656."""
+
+    @staticmethod
+    def sym_exec_gadget(gadget):
+        ctx = TritonContext()
+        ctx.setArchitecture(ARCH.X86_64)
+        ctx.enableMode(MODE.ALIGNED_MEMORY, True)
+
+        r = ctx.registers
+        gprs = (
+            r.rax, r.rbx, r.rcx, r.rdx,
+            r.rsi, r.rdi, r.esp, r.ebp,
+            r.r8, r.r9, r.r10, r.r11,
+            r.r12, r.r13, r.r14
+        )
+
+        for gpr in gprs:
+            ctx.convertRegisterToSymbolicVariable(gpr)
+
+        for assembly in gadget:
+            i = Instruction()
+            i.setOpcode(assembly)
+            ctx.processing(i)
+
+        srax = ctx.getSymbolicRegister(ctx.registers.rax)
+
+        return srax
+
+
+    def test_issue(self):
+        e = self.sym_exec_gadget((b'\x89\xd8', b'\xc2\x04\x00'))
+        self.assertEqual(str(e), '(define-fun ref!15 () (_ BitVec 64) ((_ zero_extend 32) ((_ extract 31 0) ref!1))) ; MOV operation')
+        a = e.getAst()
+        self.assertEqual(str(a + 1), '(bvadd ((_ zero_extend 32) ((_ extract 31 0) ref!1)) (_ bv1 64))')
+        self.assertEqual(str(1 + a), '(bvadd (_ bv1 64) ((_ zero_extend 32) ((_ extract 31 0) ref!1)))')
+
+
+
 class TestIssue673(unittest.TestCase):
 
     """Testing #673."""
