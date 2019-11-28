c955bf21850f645ab22b75a0955c56b59c5f9801
Jonathan Salwan <jonathan.salwan@gmail.com>
2019-11-28 10:59:27 +0100

    Review #831: Fix ID_SHIFT_RRX in symbolicEngine

--- src/libtriton/engines/symbolic/symbolicEngine.cpp
+++ src/libtriton/engines/symbolic/symbolicEngine.cpp
@@ -617,7 +617,7 @@ namespace triton {
                       this->astCtxt->bvror(
                         this->astCtxt->concat(
                           node,
-                          this->getOperandAst(this->architecture->getRegister(triton::arch::ID_REG_ARM32_C))
+                          this->getRegisterAst(this->architecture->getRegister(triton::arch::ID_REG_ARM32_C))
                         ),
                         1
                       )
@@ -679,7 +679,7 @@ namespace triton {
             /* NOTE: Capstone considers this as a viable shift operand but
              * according to the ARM manual this is not possible.
              */
-            throw triton::exceptions::SymbolicEngine("SymbolicEngine::getShiftAst(): Invalid shift operand.");
+            throw triton::exceptions::SymbolicEngine("SymbolicEngine::getShiftAst(): ID_SHIFT_RRX_REG is an invalid shift operand.");
 
           default:
             throw triton::exceptions::SymbolicEngine("SymbolicEngine::getShiftAst(): Invalid shift operand.");
