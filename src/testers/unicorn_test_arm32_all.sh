158bc7f37f9caca0cd383bf04ed3ce73ec06dbbc
Christian Heitman <cheitman@quarkslab.com>
2019-12-06 13:35:28 -0300

    Dev #831: Add tests runner for ARM

---
+++ src/testers/unicorn_test_arm32_all.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+python unicorn_test_arm32.py
+python unicorn_test_arm32_branches.py
+python unicorn_test_arm32_interworking.py
+python unicorn_test_arm32_interworking_thumb.py
+python unicorn_test_arm32_thumb.py
+python unicorn_test_arm32_thumb_branches.py
