f8bc9d89bab54a2a603588129b734360b21a6e08
Christian Heitman <cheitman@quarkslab.com>
2019-12-19 16:05:23 -0300

    Dev #831: Add tests for ARM and Thumb interworking

---
+++ src/testers/unicorn_test_arm32_interworking_thumb.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python2
+## -*- coding: utf-8 -*-
+
+from __future__          import print_function
+from triton              import *
+from unicorn             import *
+from unicorn.arm_const   import *
+from capstone            import *
+from capstone.arm_const  import *
+
+import sys
+import random
+
+
+ADDR  = 0x000000
+STACK = 0x100000
+HEAP  = 0x200000
+SIZE  = 5 * 1024 * 1024
+
+# Switchs from Thumb to ARM and back.
+CODE = [
+# 00000000 <thumb_code>:
+    (0x00, b"\x1c\x1d",         "adds    r4, r3, #4"),
+    (0x02, b"\x5c\x41",         "adcs    r4, r3"),
+    (0x04, b"\xf0\x47",         "blx     lr"),
+    (0x06, b"\x00\x00",         ".short  0x0000"),
+
+# 00000008 <arm_code>:
+    (0x08, b"\x04\x40\x83\xe2", "add     r4, r3, #4"),
+    (0x0c, b"\x04\x40\xa3\xe2", "adc     r4, r3, #4"),
+    (0x10, b"\x3e\xff\x2f\xe1", "blx     lr"),
+
+# 00000014 <_start>:
+    (0x14, b"\x88\x1c",         "adds    r0, r1, #2"),
+    (0x16, b"\x43\x41",         "adcs    r3, r0"),
+    (0x18, b"\xff\xf7\xf6\xef", "blx     8 <arm_code>"),
+    (0x1c, b"\xff\xf7\xf0\xff", "bl      0 <thumb_code>"),
+    (0x20, b"\x00\xbe",         "bkpt    0x0000"),
+    (0x22, b"\xc0\x46",         "nop                     ; (mov r8, r8)"),
+]
+
+
+def hook_code(mu, address, size, istate):
+    opcode = mu.mem_read(address, size)
+    cpsr = mu.reg_read(ARM_REG_CPSR)
+    thumb = (cpsr >> 5) & 0x1
+
+    # print("[UC] CPSR[T]: {:x}".format(thumb))
+
+    md = Cs(CS_ARCH_ARM, CS_MODE_THUMB if thumb else CS_MODE_ARM)
+    md.detail = True
+    i = list(md.disasm(opcode, address))[0]
+    disasm = "{} {}".format(i.mnemonic, i.op_str)
+
+    print("[UC] Processing: {:08x}: {}".format(address, disasm))
+
+    # print("-" * 80)
+
+def emu_with_unicorn(start, stop, istate):
+    # Initialize emulator in arm32 mode
+    mu = Uc(UC_ARCH_ARM, UC_MODE_ARM)
+
+    # map memory for this emulation
+    print("[UC] Mapping memory from {:#x} to {:#x}".format(ADDR, ADDR + SIZE));
+    mu.mem_map(ADDR, SIZE)
+
+    # write machine code to be emulated to memory
+    index = 0
+    for _, op, _ in CODE:
+        mu.mem_write(ADDR+index, op)
+        index += len(op)
+
+    apsr = mu.reg_read(UC_ARM_REG_APSR)
+    nzcv = istate['n'] << 31 | istate['z'] << 30 | istate['c'] << 29 | istate['v'] << 28
+
+    mu.mem_write(STACK,                bytes(istate['stack']))
+    mu.mem_write(HEAP,                 bytes(istate['heap']))
+    mu.reg_write(UC_ARM_REG_R0,        istate['r0'])
+    mu.reg_write(UC_ARM_REG_R1,        istate['r1'])
+    mu.reg_write(UC_ARM_REG_R2,        istate['r2'])
+    mu.reg_write(UC_ARM_REG_R3,        istate['r3'])
+    mu.reg_write(UC_ARM_REG_R4,        istate['r4'])
+    mu.reg_write(UC_ARM_REG_R5,        istate['r5'])
+    mu.reg_write(UC_ARM_REG_R6,        istate['r6'])
+    mu.reg_write(UC_ARM_REG_R7,        istate['r7'])
+    mu.reg_write(UC_ARM_REG_R8,        istate['r8'])
+    mu.reg_write(UC_ARM_REG_R9,        istate['r9'])
+    mu.reg_write(UC_ARM_REG_R10,       istate['r10'])
+    mu.reg_write(UC_ARM_REG_R11,       istate['r11'])
+    mu.reg_write(UC_ARM_REG_R12,       istate['r12'])
+    mu.reg_write(UC_ARM_REG_SP,        istate['sp'])
+    mu.reg_write(UC_ARM_REG_R14,       istate['r14'])
+    mu.reg_write(UC_ARM_REG_PC,        istate['pc'])
+    mu.reg_write(UC_ARM_REG_APSR,      apsr & 0x0fffffff | nzcv)
+
+    # tracing all instructions with customized callback
+    mu.hook_add(UC_HOOK_CODE, hook_code, user_data=istate)
+
+    # emulate code in infinite time & unlimited instructions
+    print("[UC] Executing from {:#x} to {:#x}".format(start & ~0x1, stop))
+    try:
+        mu.emu_start(start, stop)
+    except UcError as e:
+        print("[UC] Error: {}".format(e))
+
+    ostate = {
+        "stack": mu.mem_read(STACK, 0x100),
+        "heap":  mu.mem_read(HEAP, 0x100),
+        "r0":    mu.reg_read(UC_ARM_REG_R0),
+        "r1":    mu.reg_read(UC_ARM_REG_R1),
+        "r2":    mu.reg_read(UC_ARM_REG_R2),
+        "r3":    mu.reg_read(UC_ARM_REG_R3),
+        "r4":    mu.reg_read(UC_ARM_REG_R4),
+        "r5":    mu.reg_read(UC_ARM_REG_R5),
+        "r6":    mu.reg_read(UC_ARM_REG_R6),
+        "r7":    mu.reg_read(UC_ARM_REG_R7),
+        "r8":    mu.reg_read(UC_ARM_REG_R8),
+        "r9":    mu.reg_read(UC_ARM_REG_R9),
+        "r10":   mu.reg_read(UC_ARM_REG_R10),
+        "r11":   mu.reg_read(UC_ARM_REG_R11),
+        "r12":   mu.reg_read(UC_ARM_REG_R12),
+        "sp":    mu.reg_read(UC_ARM_REG_SP),
+        "r14":   mu.reg_read(UC_ARM_REG_R14),
+        "pc":    mu.reg_read(UC_ARM_REG_PC),
+        "n":   ((mu.reg_read(UC_ARM_REG_APSR) >> 31) & 1),
+        "z":   ((mu.reg_read(UC_ARM_REG_APSR) >> 30) & 1),
+        "c":   ((mu.reg_read(UC_ARM_REG_APSR) >> 29) & 1),
+        "v":   ((mu.reg_read(UC_ARM_REG_APSR) >> 28) & 1),
+    }
+    return ostate
+
+def emu_with_triton(start, stop, istate):
+    ctx = TritonContext()
+    ctx.setArchitecture(ARCH.ARM32)
+
+    ctx.setConcreteMemoryAreaValue(STACK,           bytes(istate['stack']))
+    ctx.setConcreteMemoryAreaValue(HEAP,            bytes(istate['heap']))
+    ctx.setConcreteRegisterValue(ctx.registers.r0,  istate['r0'])
+    ctx.setConcreteRegisterValue(ctx.registers.r1,  istate['r1'])
+    ctx.setConcreteRegisterValue(ctx.registers.r2,  istate['r2'])
+    ctx.setConcreteRegisterValue(ctx.registers.r3,  istate['r3'])
+    ctx.setConcreteRegisterValue(ctx.registers.r4,  istate['r4'])
+    ctx.setConcreteRegisterValue(ctx.registers.r5,  istate['r5'])
+    ctx.setConcreteRegisterValue(ctx.registers.r6,  istate['r6'])
+    ctx.setConcreteRegisterValue(ctx.registers.r7,  istate['r7'])
+    ctx.setConcreteRegisterValue(ctx.registers.r8,  istate['r8'])
+    ctx.setConcreteRegisterValue(ctx.registers.r9,  istate['r9'])
+    ctx.setConcreteRegisterValue(ctx.registers.r10, istate['r10'])
+    ctx.setConcreteRegisterValue(ctx.registers.r11, istate['r11'])
+    ctx.setConcreteRegisterValue(ctx.registers.r12, istate['r12'])
+    ctx.setConcreteRegisterValue(ctx.registers.sp,  istate['sp'])
+    ctx.setConcreteRegisterValue(ctx.registers.r14, istate['r14'])
+    ctx.setConcreteRegisterValue(ctx.registers.pc,  istate['pc'])
+    ctx.setConcreteRegisterValue(ctx.registers.n,   istate['n'])
+    ctx.setConcreteRegisterValue(ctx.registers.z,   istate['z'])
+    ctx.setConcreteRegisterValue(ctx.registers.c,   istate['c'])
+    ctx.setConcreteRegisterValue(ctx.registers.v,   istate['v'])
+
+    code = {}
+    for addr, opcode, disasm in CODE:
+        code[addr] = (opcode, disasm)
+
+    addr = start & ~0x1
+    while addr != stop:
+        # print("[TT] Fetching instruction at address: {:08x}".format(addr))
+
+        opcode, disasm = code[addr]
+
+        print("[TT] Processing: {:08x}: {}".format(addr, disasm))
+
+        inst = Instruction(opcode)
+
+        inst.setAddress(addr)
+
+        ctx.processing(inst)
+
+        # print()
+        # print(inst)
+        # for x in inst.getSymbolicExpressions():
+        #    print(x)
+        # print()
+
+        addr = ctx.getSymbolicRegisterValue(ctx.registers.pc) & ~0x1
+
+        # print("-" * 80)
+
+    ostate = {
+        "stack": ctx.getConcreteMemoryAreaValue(STACK, 0x100),
+        "heap":  ctx.getConcreteMemoryAreaValue(HEAP, 0x100),
+        "r0":    ctx.getSymbolicRegisterValue(ctx.registers.r0),
+        "r1":    ctx.getSymbolicRegisterValue(ctx.registers.r1),
+        "r2":    ctx.getSymbolicRegisterValue(ctx.registers.r2),
+        "r3":    ctx.getSymbolicRegisterValue(ctx.registers.r3),
+        "r4":    ctx.getSymbolicRegisterValue(ctx.registers.r4),
+        "r5":    ctx.getSymbolicRegisterValue(ctx.registers.r5),
+        "r6":    ctx.getSymbolicRegisterValue(ctx.registers.r6),
+        "r7":    ctx.getSymbolicRegisterValue(ctx.registers.r7),
+        "r8":    ctx.getSymbolicRegisterValue(ctx.registers.r8),
+        "r9":    ctx.getSymbolicRegisterValue(ctx.registers.r9),
+        "r10":   ctx.getSymbolicRegisterValue(ctx.registers.r10),
+        "r11":   ctx.getSymbolicRegisterValue(ctx.registers.r11),
+        "r12":   ctx.getSymbolicRegisterValue(ctx.registers.r12),
+        "sp":    ctx.getSymbolicRegisterValue(ctx.registers.sp),
+        "r14":   ctx.getSymbolicRegisterValue(ctx.registers.r14),
+        "pc":    ctx.getSymbolicRegisterValue(ctx.registers.pc) & ~0x1,
+        "n":     ctx.getSymbolicRegisterValue(ctx.registers.n),
+        "z":     ctx.getSymbolicRegisterValue(ctx.registers.z),
+        "c":     ctx.getSymbolicRegisterValue(ctx.registers.c),
+        "v":     ctx.getSymbolicRegisterValue(ctx.registers.v),
+    }
+    return ostate
+
+def diff_state(state1, state2):
+    for k, v in list(state1.items()):
+        if (k == 'heap' or k == 'stack') and v != state2[k]:
+            print('\t%s: (UC) != (TT)' %(k))
+        elif not (k == 'heap' or k == 'stack') and v != state2[k]:
+            print('\t%s: %#x (UC) != %#x (TT)' %(k, v, state2[k]))
+    return
+
+def print_state(istate, uc_ostate, tt_ostate):
+    for k in sorted(istate.keys()):
+        if k in ['stack', 'heap']:
+            continue
+
+        diff = "!=" if uc_ostate[k] != tt_ostate[k] else "=="
+
+        print("{:>3s}: {:08x} | {:08x} {} {:08x}".format(k, istate[k], uc_ostate[k], diff, tt_ostate[k]))
+
+
+if __name__ == '__main__':
+    start = 0x14 | 1    # Address of _start function.
+    stop  = 0x20        # Address of the last instruction of _start function.
+
+    # initial state
+    state = {
+        "stack": b"".join([bytes(255 - i) for i in range(256)]),
+        "heap":  b"".join([bytes(i) for i in range(256)]),
+        "r0":    random.randint(0x0, 0xffffffff),
+        "r1":    random.randint(0x0, 0xffffffff),
+        "r2":    random.randint(0x0, 0xffffffff),
+        "r3":    random.randint(0x0, 0xffffffff),
+        "r4":    random.randint(0x0, 0xffffffff),
+        "r5":    random.randint(0x0, 0xffffffff),
+        "r6":    random.randint(0x0, 0xffffffff),
+        "r7":    random.randint(0x0, 0xffffffff),
+        "r8":    random.randint(0x0, 0xffffffff),
+        "r9":    random.randint(0x0, 0xffffffff),
+        "r10":   random.randint(0x0, 0xffffffff),
+        "r11":   random.randint(0x0, 0xffffffff),
+        "r12":   random.randint(0x0, 0xffffffff),
+        "sp":    STACK,
+        "r14":   random.randint(0x0, 0xffffffff),
+        "pc":    start,
+        "n":     random.randint(0x0, 0x1),
+        "z":     random.randint(0x0, 0x1),
+        "c":     random.randint(0x0, 0x1),
+        "v":     random.randint(0x0, 0x1),
+    }
+
+    try:
+        uc_state = emu_with_unicorn(start, stop, state)
+        print("~" * 80)
+        tt_state = emu_with_triton(start, stop, state)
+    except Exception as e:
+        print('[EE] \t%s' %(e))
+        sys.exit(-1)
+
+    if uc_state != tt_state:
+        print("[EE] Execution differs!")
+        diff_state(uc_state, tt_state)
+        print_state(state, uc_state, tt_state)
+        sys.exit(-1)
+
+    sys.exit(0)
