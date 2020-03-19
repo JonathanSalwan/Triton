b7ebfb530b7f6fc03fb8f3575d59225b4f59eae1
Jonathan Salwan <jonathan.salwan@gmail.com>
2020-03-19 09:27:32 +0100

    Update example about synthetizing

--- src/examples/python/synthetizing_obfuscated_expressions.py
+++ src/examples/python/synthetizing_obfuscated_expressions.py
@@ -3,24 +3,30 @@
 ##
 ## Example of synthetizing obfuscated expressions.
 ##
-## $ python synthetizing_obfuscated_expressions.py
-## In: (bvsub (bvadd (bvor SymVar_0 SymVar_1) SymVar_1) (bvand (bvnot SymVar_0) SymVar_1))
-## Out: (bvadd SymVar_0 SymVar_1)
+## $ python ./synthetizing_obfuscated_expressions.py
+## In: (((((SymVar_0 | SymVar_1) + SymVar_1) & 0xff) - ((~(SymVar_0) & 0xff) & SymVar_1)) & 0xff)
+## Out: ((SymVar_0 + SymVar_1) & 0xff)
 ##
-## In: (bvadd (bvsub (bvor SymVar_0 SymVar_1) SymVar_1) (bvand (bvnot SymVar_0) SymVar_1))
-## Out: (bvxor SymVar_0 SymVar_1)
+## In: (((((SymVar_0 | SymVar_1) - SymVar_1) & 0xff) + ((~(SymVar_0) & 0xff) & SymVar_1)) & 0xff)
+## Out: (SymVar_0 ^ SymVar_1)
 ##
-## In: (bvor (bvand SymVar_0 (bvnot SymVar_1)) (bvand (bvnot SymVar_0) SymVar_1))
-## Out: (bvxor SymVar_0 SymVar_1)
+## In: ((SymVar_0 & (~(SymVar_1) & 0xff)) | ((~(SymVar_0) & 0xff) & SymVar_1))
+## Out: (SymVar_0 ^ SymVar_1)
 ##
-## In: (bvsub (bvadd (bvxor SymVar_0 SymVar_1) SymVar_1) (bvand (bvnot SymVar_0) SymVar_1))
-## Out: (bvor SymVar_0 SymVar_1)
+## In: (((((SymVar_0 ^ SymVar_1) + SymVar_1) & 0xff) - ((~(SymVar_0) & 0xff) & SymVar_1)) & 0xff)
+## Out: (SymVar_0 | SymVar_1)
 ##
-## In: (bvadd (bvadd (bvneg (bvor SymVar_0 SymVar_1)) SymVar_1) SymVar_0)
-## Out: (bvand SymVar_1 SymVar_0)
+## In: ((((-(SymVar_0 | SymVar_1) + SymVar_1) & 0xff) + SymVar_0) & 0xff)
+## Out: (SymVar_1 & SymVar_0)
 ##
-## In: (bvshl (bvlshr (bvshl SymVar_2 (_ bv8 32)) (_ bv16 32)) (_ bv8 32))
-## Out: (bvand SymVar_2 (_ bv16776960 32))
+## In: (((((SymVar_2 << 0x8) & 0xffffffff) >> 0x10) << 0x8) & 0xffffffff)
+## Out: (SymVar_2 & 0xffff00)
+##
+## In: (((((((((((SymVar_0 ^ SymVar_1) + ((0x2 * (SymVar_0 & SymVar_1)) & 0xff)) & 0xff) * 0x27) & 0xff) +  ...
+## Out: ((SymVar_0 + SymVar_1) & 0xff)
+##
+## In: (((0xed * ((((((((0x2d * ((((((((((((((((((0x3a * (((((((((((0x56 * ((((((((((((0xed * ((((0xe5 * Sy ...
+## Out: (SymVar_0 ^ 0x5c)
 ##
 
 import sys
@@ -129,9 +135,27 @@ def synthetize(ctx, expr):
     return expr
 
 
+# The following function returns an MBA-obfuscated expression found by
+# Mougey et al. while analyzing an obfuscated program. I take this
+# representation from the Ninon Eyrolle's thesis. The MBA performs
+# the following operation: (x ^ 92).
+def x_xor_92_obfuscated(x):
+    a = 229 * x + 247
+    b = 237 * a + 214 + ((38 * a + 85) & 254)
+    c = (b + ((-(2 * b) + 255) & 254)) * 3 + 77
+    d = ((86 * c + 36) & 70) * 75 + 231 * c + 118
+    e = ((58 * d + 175) & 244) + 99 * d + 46
+    f = (e & 148)
+    g = (f - (e & 255) + f) * 103 + 13
+    R = (237 * (45 * g + (174 * g | 34) * 229 + 194 - 247) & 255)
+    return R
+
+
 def main():
     ctx = TritonContext(ARCH.X86_64)
     ast = ctx.getAstContext()
+
+    ctx.setAstRepresentationMode(AST_REPRESENTATION.PYTHON)
 
     x = ast.variable(ctx.newSymbolicVariable(8))
     y = ast.variable(ctx.newSymbolicVariable(8))
@@ -139,16 +163,18 @@ def main():
 
     # Some obfuscated expressions
     obf_exprs = [
-        (x | y) + y - (~x & y), # x + y
-        (x | y) - y + (~x & y), # x ^ y
-        (x & ~y) | (~x & y),    # x ^ y
-        (x ^ y) + y - (~x & y), # x | y
-        -(x | y) + y + x,       # x & y
-        ((z << 8) >> 16) << 8,  # z & 0xffff00
+        (x | y) + y - (~x & y),                             # x + y         (from http://archive.bar/pdfs/bar2020-preprint9.pdf)
+        (x | y) - y + (~x & y),                             # x ^ y         (from http://archive.bar/pdfs/bar2020-preprint9.pdf)
+        (x & ~y) | (~x & y),                                # x ^ y         (from ?)
+        (x ^ y) + y - (~x & y),                             # x | y         (from http://archive.bar/pdfs/bar2020-preprint9.pdf)
+        -(x | y) + y + x,                                   # x & y         (from http://archive.bar/pdfs/bar2020-preprint9.pdf)
+        ((z << 8) >> 16) << 8,                              # z & 0xffff00  (from https://blog.regehr.org/archives/1636)
+        (((x ^ y) + 2 * (x & y)) * 39 + 23) * 151 + 111,    # x + y         (from Ninon Eyrolle's thesis)
+        x_xor_92_obfuscated(x),                             # x ^ 92        (from Ninon Eyrolle's thesis)
     ]
 
     for expr in obf_exprs:
-        print('In: %s' %(expr))
+        (print('In: %s' %(expr)) if len(str(expr)) < 100 else print('In: %s ...' %(str(expr)[0:100])))
         expr = synthetize(ctx, expr)
         print('Out: %s' %(expr))
         print()
