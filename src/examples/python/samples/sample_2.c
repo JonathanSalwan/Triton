5c117e2c3bde7485ec5613215edfacbaab45f82c
EpureRares <rares_stefan1999@yahoo.ro>
2021-10-18 15:55:03 -0400

    Fix printf example.

---
+++ src/examples/python/samples/sample_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+
+   printf("Some numbers and %s (%d,%d,%d,%d,%d) (%c) (%s) (%d)\n", "some strings", 1,2,3,4,5,'c', "abcdefg", 323);
+
+   return(0);
+}
