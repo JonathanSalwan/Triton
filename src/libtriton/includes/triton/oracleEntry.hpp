7d296f148276626465342ae9b0c85dfeeafe0145
Jonathan Salwan <jonathan.salwan@gmail.com>
2022-01-26 12:13:53 +0100

    Add the possibility to add trinary operators synth

--- src/libtriton/includes/triton/oracleEntry.hpp
+++ src/libtriton/includes/triton/oracleEntry.hpp
@@ -45,9 +45,11 @@ namespace triton {
           //! \brief Example: bvadd
           triton::ast::SharedAbstractNode op;
 
-          //! Constant position in the AST
-          //! \brief Example 1: c - x -> 0
-          //! \brief Example 2: x - c -> 1
+          /*! Constant position in the AST
+           * \brief
+           * Example 1: c - x -> 0
+           * Example 2: x - c -> 1
+           */
           triton::uint8 position;
 
           //! Constructor
@@ -97,26 +99,47 @@ namespace triton {
           };
       };
 
-      //! \class OpEncoding
-      /*! \brief This class is used to encode operators and variables ordering. */
-      /*! \brief Example 1: ((a + b) * a)     -> OpEncoding<2>({BVADD, BVMUL}, {0, 1, 0})           */
-      /*! \brief Example 2: (a + (b * a))     -> OpEncoding<2>({BVMUL, BVADD}, {1, 0, 0})           */
-      /*! \brief Example 3: (a + b) * (a - b) -> OpEncoding<3>({BVMUL, BVADD, BVSUB}, {0, 1, 0, 1}) */
+      /*! \class OpEncoding
+       * \brief This class is used to encode operators and variables ordering.
+       * \brief Example 1: ((a + b) * a)     -> OpEncoding<2>({BVADD, BVMUL}, {0, 1, 0})
+       * \brief Example 2: (a + (b * a))     -> OpEncoding<2>({BVMUL, BVADD}, {1, 0, 0})
+       * \brief Example 3: (a + b) * (a - b) -> OpEncoding<3>({BVMUL, BVADD, BVSUB}, {0, 1, 0, 1})
+       *
+       * Example for a trinary operator like bvadd(bvmul(x, x), y):
+       * {
+       *   OpEncoding<2>({triton::ast::BVADD_NODE, triton::ast::BVMUL_NODE}, {0, 0, 1}), {
+       *     BinaryEntry(8, 0x74, 0xe7, 0x77), BinaryEntry(16, 0x04c3, 0xd4d4, 0x815d), ...
+       *     BinaryEntry(8, 0x98, 0x13, 0x53), BinaryEntry(16, 0x46dd, 0xc2dd, 0x5da6), ...
+       *     BinaryEntry(8, 0x3f, 0xf4, 0x75), BinaryEntry(16, 0xa6b7, 0xac75, 0x8346), ...
+       *     BinaryEntry(8, 0x26, 0xf0, 0x94), BinaryEntry(16, 0x31cd, 0x33c5, 0x51ee), ...
+       *     BinaryEntry(8, 0x8e, 0x5e, 0x22), BinaryEntry(16, 0x77b8, 0xbba1, 0x4fe1), ...
+       *     BinaryEntry(8, 0x48, 0x7c, 0xbc), BinaryEntry(16, 0x1c89, 0xed45, 0x2e96), ...
+       *     BinaryEntry(8, 0x9f, 0x7b, 0x3c), BinaryEntry(16, 0xbfcc, 0xd906, 0xe396), ...
+       *     BinaryEntry(8, 0x45, 0xe3, 0x7c), BinaryEntry(16, 0xd200, 0xb90c, 0xb90c), ...
+       *     BinaryEntry(8, 0x16, 0x0a, 0xee), BinaryEntry(16, 0xd946, 0x9d22, 0x5c46), ...
+       *     BinaryEntry(8, 0xed, 0x59, 0xc2), BinaryEntry(16, 0x1c7b, 0x810c, 0xa425), ...
+       *   }
+       * }
+       */
       template<std::size_t N>
       class OpEncoding {
         public:
-          //! Ordering of operators
-          /*! \brief Example 1: ((a + b) * a) -> {BVADD, BVMUL} */
-          /*! \brief Example 2: (a + (b * a)) -> {BVMUL, BVADD} */
-          /*! \brief Example 3: (a + (b + a)) -> {BVADD, BVADD} */
+          /*! Ordering of operators
+           * \brief
+           * Example 1: ((a + b) * a) -> {BVADD, BVMUL}
+           * Example 2: (a + (b * a)) -> {BVMUL, BVADD}
+           * Example 3: (a + (b + a)) -> {BVADD, BVADD}
+           */
           std::array<triton::ast::ast_e, N> ops;
 
-          //! Ordering of variables
-          /*! \brief Example 1: ((a + b) * a) -> {0, 1, 0} */
-          /*! \brief Example 2: ((a + a) * a) -> {0, 0, 0} */
-          /*! \brief Example 3: ((b + b) * a) -> {1, 1, 0} */
-          /*! \brief Example 4: ((b + a) * b) -> {1, 0, 1} */
-          /*! \brief Example 4: (b + (a * b)) -> {0, 1, 1} */
+          /*! Ordering of variables
+           * \brief
+           * Example 1: ((a + b) * a) -> {0, 1, 0}
+           * Example 2: ((a + a) * a) -> {0, 0, 0}
+           * Example 3: ((b + b) * a) -> {1, 1, 0}
+           * Example 4: ((b + a) * b) -> {1, 0, 1}
+           * Example 4: (b + (a * b)) -> {0, 1, 1}
+           */
           std::array<triton::uint8, N + 1> vars;
 
           //! Constructor
--- src/scripts/gen_oracle_table.py
+++ src/scripts/gen_oracle_table.py
@@ -12,7 +12,6 @@ import sys
 import operator
 
 from triton import *
-from ctypes import c_uint8, c_uint16, c_uint32, c_uint64
 from random import randrange
 
 HOW_BIG_IS_THE_TABLE = 10
@@ -47,111 +46,185 @@ binary_operators = [
 ]
 
 
-def gen_unary_operator(unary_op):
-    op, name, enum = unary_op
-    print('          /* %s synthesis */' %(name))
-    print('          {')
-    print('            %s, {' %(enum))
-    for i in range(HOW_BIG_IS_THE_TABLE):
-        stop = False
-        while not stop:
-            s1 = ast.bv(randrange(1, 0x100), 8)
-            r1 = op(s1)
-            if r1.evaluate() != 0:
-                stop = True
+def gen_unary_operator():
+    for op, name, enum in unary_operators:
+        print('          /* %s synthesis */' %(name))
+        print('          {')
+        print('            %s, {' %(enum))
+        for i in range(HOW_BIG_IS_THE_TABLE):
+            stop = False
+            while not stop:
+                s1 = ast.bv(randrange(1, 0x100), 8)
+                r1 = op(s1)
+                if r1.evaluate() != 0:
+                    stop = True
 
-        stop = False
-        while not stop:
-            s2 = ast.bv(randrange(0x100, 0x10000), 16)
-            r2 = op(s2)
-            if r2.evaluate() != 0:
-                stop = True
+            stop = False
+            while not stop:
+                s2 = ast.bv(randrange(0x100, 0x10000), 16)
+                r2 = op(s2)
+                if r2.evaluate() != 0:
+                    stop = True
 
-        stop = False
-        while not stop:
-            s3 = ast.bv(randrange(0x10000, 0x100000000), 32)
-            r3 = op(s3)
-            if r3.evaluate() != 0:
-                stop = True
+            stop = False
+            while not stop:
+                s3 = ast.bv(randrange(0x10000, 0x100000000), 32)
+                r3 = op(s3)
+                if r3.evaluate() != 0:
+                    stop = True
 
-        stop = False
-        while not stop:
-            s4 = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
-            r4 = op(s4)
-            if r4.evaluate() != 0:
-                stop = True
+            stop = False
+            while not stop:
+                s4 = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
+                r4 = op(s4)
+                if r4.evaluate() != 0:
+                    stop = True
 
-        print('              UnaryEntry(8, 0x%02x, 0x%02x), UnaryEntry(16, 0x%04x, 0x%04x), UnaryEntry(32, 0x%08x, 0x%08x), UnaryEntry(64, 0x%016x, 0x%016x),'
-            % (s1.evaluate(), r1.evaluate(), s2.evaluate(), r2.evaluate(), s3.evaluate(), r3.evaluate(), s4.evaluate(), r4.evaluate())
-        )
-    print('            }')
-    print('          },')
+            print('              UnaryEntry(8, 0x%02x, 0x%02x), UnaryEntry(16, 0x%04x, 0x%04x), UnaryEntry(32, 0x%08x, 0x%08x), UnaryEntry(64, 0x%016x, 0x%016x),'
+                % (s1.evaluate(), r1.evaluate(), s2.evaluate(), r2.evaluate(), s3.evaluate(), r3.evaluate(), s4.evaluate(), r4.evaluate())
+            )
+        print('            }')
+        print('          },')
     return
 
 
-def gen_binary_operator(binary_op):
-    op, name, enum = binary_op
-    print('          /* %s synthesis */' %(name))
-    print('          {')
-    print('            %s, {' %(enum))
-    for i in range(HOW_BIG_IS_THE_TABLE):
-        stop = False
-        while not stop:
-            s1 = ast.bv(randrange(1, 0x100), 8)
-            s2 = ast.bv(randrange(1, 0x100), 8)
+def gen_binary_operator():
+    for op, name, enum in binary_operators:
+        print('          /* %s synthesis */' %(name))
+        print('          {')
+        print('            %s, {' %(enum))
+        for i in range(HOW_BIG_IS_THE_TABLE):
+            stop = False
+            while not stop:
+                s1 = ast.bv(randrange(1, 0x100), 8)
+                s2 = ast.bv(randrange(1, 0x100), 8)
 
-            # Special case for div, we need small number
-            if name in ['bvsdiv', 'bvudiv']:
-                s2 = ast.bv(1 << i % 7, 8)
+                # Special case for div, we need small number
+                if name in ['bvsdiv', 'bvudiv']:
+                    s2 = ast.bv(1 << i % 7, 8)
 
-            r1 = op(s1, s2)
-            if r1.evaluate() != 0:
-                stop = True
+                r1 = op(s1, s2)
+                if r1.evaluate() != 0:
+                    stop = True
 
-        stop = False
-        while not stop:
-            s3 = ast.bv(randrange(0x100, 0x10000), 16)
-            s4 = ast.bv(randrange(0x100, 0x10000), 16)
+            stop = False
+            while not stop:
+                s3 = ast.bv(randrange(0x100, 0x10000), 16)
+                s4 = ast.bv(randrange(0x100, 0x10000), 16)
 
-            # Special case for div, we need small number
-            if name in ['bvsdiv', 'bvudiv']:
-                s4 = ast.bv(1 << i, 16)
+                # Special case for div, we need small number
+                if name in ['bvsdiv', 'bvudiv']:
+                    s4 = ast.bv(1 << i, 16)
 
-            r2 = op(s3, s4)
-            if r2.evaluate() != 0:
-                stop = True
+                r2 = op(s3, s4)
+                if r2.evaluate() != 0:
+                    stop = True
 
-        stop = False
-        while not stop:
-            s5 = ast.bv(randrange(0x10000, 0x100000000), 32)
-            s6 = ast.bv(randrange(0x10000, 0x100000000), 32)
+            stop = False
+            while not stop:
+                s5 = ast.bv(randrange(0x10000, 0x100000000), 32)
+                s6 = ast.bv(randrange(0x10000, 0x100000000), 32)
 
-            # Special case for div, we need small number
-            if name in ['bvsdiv', 'bvudiv']:
-                s6 = ast.bv(1 << i, 32)
+                # Special case for div, we need small number
+                if name in ['bvsdiv', 'bvudiv']:
+                    s6 = ast.bv(1 << i, 32)
 
-            r3 = op(s5, s6)
-            if r3.evaluate() != 0:
-                stop = True
+                r3 = op(s5, s6)
+                if r3.evaluate() != 0:
+                    stop = True
 
-        stop = False
-        while not stop:
-            s7 = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
-            s8 = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
+            stop = False
+            while not stop:
+                s7 = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
+                s8 = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
 
-            # Special case for div, we need small number
-            if name in ['bvsdiv', 'bvudiv']:
-                s8 = ast.bv(1 << i, 64)
+                # Special case for div, we need small number
+                if name in ['bvsdiv', 'bvudiv']:
+                    s8 = ast.bv(1 << i, 64)
 
-            r4 = op(s7, s8)
-            if r4.evaluate() != 0:
-                stop = True
+                r4 = op(s7, s8)
+                if r4.evaluate() != 0:
+                    stop = True
 
-        print('              BinaryEntry(8, 0x%02x, 0x%02x, 0x%02x), BinaryEntry(16, 0x%04x, 0x%04x, 0x%04x), BinaryEntry(32, 0x%08x, 0x%08x, 0x%08x), BinaryEntry(64, 0x%016x, 0x%016x, 0x%016x),'
-            % (s1.evaluate(), s2.evaluate(), r1.evaluate(), s3.evaluate(), s4.evaluate(), r2.evaluate(), s5.evaluate(), s6.evaluate(), r3.evaluate(), s7.evaluate(), s8.evaluate(), r4.evaluate())
-        )
-    print('            }')
-    print('          },')
+            print('              BinaryEntry(8, 0x%02x, 0x%02x, 0x%02x), BinaryEntry(16, 0x%04x, 0x%04x, 0x%04x), BinaryEntry(32, 0x%08x, 0x%08x, 0x%08x), BinaryEntry(64, 0x%016x, 0x%016x, 0x%016x),'
+                % (s1.evaluate(), s2.evaluate(), r1.evaluate(), s3.evaluate(), s4.evaluate(), r2.evaluate(), s5.evaluate(), s6.evaluate(), r3.evaluate(), s7.evaluate(), s8.evaluate(), r4.evaluate())
+            )
+        print('            }')
+        print('          },')
+    return
+
+
+def gen_trinary_operator():
+    vk = {0: 'x', 1: 'y'}
+    for op1, name1, enum1 in binary_operators:
+        for op2, name2, enum2 in binary_operators:
+            for s1 in [0, 1]:
+                for s2 in [0, 1]:
+                    for s3 in [0, 1]:
+
+                        # Skip expressions that contain only one variable
+                        if s1 == s2 == s3:
+                            continue
+
+                        # Skip those expressions
+                        subset = ['bvsdiv', 'bvsmod', 'bvsrem', 'bvudiv', 'bvurem']
+                        if name1 in subset or name2 in subset:
+                            continue
+
+                        pp = '%s(%s(%c, %c), %c)' % (name1, name2, vk[s1], vk[s2], vk[s3])
+                        print('          /* %s synthesis */' %(pp))
+                        print('          {')
+                        print('            OpEncoding<2>({%s, %s}, {%d, %d, %d}), {' % (enum1, enum2, s1, s2, s3))
+                        for i in range(HOW_BIG_IS_THE_TABLE):
+                            v1 = {}
+                            v2 = {}
+                            v3 = {}
+                            v4 = {}
+                            try_hard = 0
+                            stop = False
+                            while not stop:
+                                v1[0] = ast.bv(randrange(1, 0x100), 8)
+                                v1[1] = ast.bv(randrange(1, 0x100), 8)
+                                r1 = op1(op2(v1[s1], v1[s2]), v1[s3])
+                                if r1.evaluate() != 0 or try_hard == 1000:
+                                    stop = True
+                                try_hard += 1
+
+                            try_hard = 0
+                            stop = False
+                            while not stop:
+                                v2[0] = ast.bv(randrange(0x100, 0x10000), 16)
+                                v2[1] = ast.bv(randrange(0x100, 0x10000), 16)
+                                r2 = op1(op2(v2[s1], v2[s2]), v2[s3])
+                                if r2.evaluate() != 0 or try_hard == 1000:
+                                    stop = True
+                                try_hard += 1
+
+                            try_hard = 0
+                            stop = False
+                            while not stop:
+                                v3[0] = ast.bv(randrange(0x10000, 0x100000000), 32)
+                                v3[1] = ast.bv(randrange(0x10000, 0x100000000), 32)
+                                r3 = op1(op2(v3[s1], v3[s2]), v3[s3])
+                                if r3.evaluate() != 0 or try_hard == 1000:
+                                    stop = True
+                                try_hard += 1
+
+                            try_hard = 0
+                            stop = False
+                            while not stop:
+                                v4[0] = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
+                                v4[1] = ast.bv(randrange(0x100000000, 0x10000000000000000), 64)
+                                r4 = op1(op2(v4[s1], v4[s2]), v4[s3])
+                                if r4.evaluate() != 0 or try_hard == 1000:
+                                    stop = True
+                                try_hard += 1
+
+                            print('              BinaryEntry(8, 0x%02x, 0x%02x, 0x%02x), BinaryEntry(16, 0x%04x, 0x%04x, 0x%04x), BinaryEntry(32, 0x%08x, 0x%08x, 0x%08x), BinaryEntry(64, 0x%016x, 0x%016x, 0x%016x),'
+                                % (v1[0].evaluate(), v1[1].evaluate(), r1.evaluate(), v2[0].evaluate(), v2[1].evaluate(), r2.evaluate(), v3[0].evaluate(), v3[1].evaluate(), r3.evaluate(), v4[0].evaluate(), v4[1].evaluate(), r4.evaluate())
+                            )
+                        print('            }')
+                        print('          },')
     return
 
 
@@ -180,17 +253,23 @@ def main():
     print('        //! The oracle table for unary operators. Each entry is a UnaryEntry object.')
     print('        /*! \\brief Entry: <bits> <x value> <result> <operator> */')
     print('        std::map<triton::ast::ast_e, std::array<UnaryEntry, 40>> unopTable = {')
-    for op in unary_operators:
-        gen_unary_operator(op)
-    print('        };\n')
+    gen_unary_operator()
+    print('        };')
+    print('')
     print('')
     print('        //! The oracle table for binary operators. Each entry is a BinaryEntry object.')
     print('        /*! \\brief Entry: <bits> <x value> <y value> <result> <operator> */')
     print('        std::map<triton::ast::ast_e, std::array<BinaryEntry, 40>> binopTable = {')
-    for op in binary_operators:
-        gen_binary_operator(op)
+    gen_binary_operator()
     print('        };')
     print('')
+    #print('')
+    #print('        //! The oracle table for trinary operators. Each entry is a BinaryEntry object.')
+    #print('        /*! \\brief Entry: <bits> <x value> <y value> <result> <operator> */')
+    #print('        std::map<OpEncoding<2>, std::array<BinaryEntry, 40>> triopTable = {')
+    #gen_trinary_operator()
+    #print('        };')
+    #print('')
     print('      }; /* oracles namespace */')
     print('    }; /* synthesis namespace */')
     print('  }; /* engines namespace */')
