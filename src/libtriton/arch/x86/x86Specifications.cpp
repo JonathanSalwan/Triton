8b2d0292dfb00f39bcea32b85ee1b2866f45eb83
Jonathan Salwan <jonathan.salwan@gmail.com>
2022-05-27 13:26:49 +0200

    Coding style

--- src/libtriton/arch/x86/x86Specifications.cpp
+++ src/libtriton/arch/x86/x86Specifications.cpp
@@ -155,11 +155,11 @@ namespace triton {
             tritonId = triton::arch::x86::ID_INS_FIADD;
             break;
 
-#if CS_API_MAJOR < 5
+          #if CS_API_MAJOR < 5
           case triton::extlibs::capstone::X86_INS_FADDP:
             tritonId = triton::arch::x86::ID_INS_FADDP;
             break;
-#endif
+          #endif
 
           case triton::extlibs::capstone::X86_INS_ADOX:
             tritonId = triton::arch::x86::ID_INS_ADOX;
@@ -2881,11 +2881,11 @@ namespace triton {
             tritonId = triton::arch::x86::ID_INS_FUCOM;
             break;
 
-#if CS_API_MAJOR < 5
+          #if CS_API_MAJOR < 5
           case triton::extlibs::capstone::X86_INS_UD2B:
             tritonId = triton::arch::x86::ID_INS_UD2B;
             break;
-#endif
+          #endif
 
           case triton::extlibs::capstone::X86_INS_UNPCKHPD:
             tritonId = triton::arch::x86::ID_INS_UNPCKHPD;
@@ -3043,21 +3043,21 @@ namespace triton {
             tritonId = triton::arch::x86::ID_INS_VCVTDQ2PS;
             break;
 
-#if CS_API_MAJOR < 5
+          #if CS_API_MAJOR < 5
           case triton::extlibs::capstone::X86_INS_VCVTPD2DQX:
             tritonId = triton::arch::x86::ID_INS_VCVTPD2DQX;
             break;
-#endif
+          #endif
 
           case triton::extlibs::capstone::X86_INS_VCVTPD2DQ:
             tritonId = triton::arch::x86::ID_INS_VCVTPD2DQ;
             break;
 
-#if CS_API_MAJOR < 5
+          #if CS_API_MAJOR < 5
           case triton::extlibs::capstone::X86_INS_VCVTPD2PSX:
             tritonId = triton::arch::x86::ID_INS_VCVTPD2PSX;
             break;
-#endif
+          #endif
 
           case triton::extlibs::capstone::X86_INS_VCVTPD2PS:
             tritonId = triton::arch::x86::ID_INS_VCVTPD2PS;
@@ -3103,11 +3103,11 @@ namespace triton {
             tritonId = triton::arch::x86::ID_INS_VCVTSS2USI;
             break;
 
-#if CS_API_MAJOR < 5
+          #if CS_API_MAJOR < 5
           case triton::extlibs::capstone::X86_INS_VCVTTPD2DQX:
             tritonId = triton::arch::x86::ID_INS_VCVTTPD2DQX;
             break;
-#endif
+          #endif
 
           case triton::extlibs::capstone::X86_INS_VCVTTPD2DQ:
             tritonId = triton::arch::x86::ID_INS_VCVTTPD2DQ;
