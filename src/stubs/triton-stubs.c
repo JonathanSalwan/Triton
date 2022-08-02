c502f173f6372728e6012751545723f11b52365d
Jonathan Salwan <jonathan.salwan@gmail.com>
2022-08-02 10:56:59 +0200

    Update stub

--- src/stubs/triton-stubs.c
+++ src/stubs/triton-stubs.c
@@ -1,13 +1,13 @@
 /*
  *
- * These stubs come from the uClibc project and aim to provide standalone
- * routines helping the dynamic symbolic exploration when doing a pure
- * emulation with Triton.
+ * These stubs come from the glibc, uClibc, libiberty, berkeleydb  projects and aim
+ * to provide standalone routines helping the dynamic symbolic exploration when
+ * doing a pure emulation with Triton.
  *
  */
 
 #include <stddef.h>
-#include <stdlib.h>
+#include <limits.h>
 
 
 __attribute__((STUB_ABI))
@@ -628,4 +628,364 @@ char* dirname(char* path) {
   }
   DOT:
     return (char *)null_or_empty_or_noslash;
+}
+
+
+__attribute__((STUB_ABI))
+int isdigit(char c) {
+  return c >= '0' && c <= '9';
+}
+
+
+__attribute__((STUB_ABI))
+int isalpha(int c) {
+  return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+
+__attribute__((STUB_ABI))
+int isupper(int c) {
+  return (c >= 'A' && c <= 'Z');
+}
+
+
+__attribute__((STUB_ABI))
+int islower(int c) {
+  return (c >= 'a' && c <= 'z');
+}
+
+
+__attribute__((STUB_ABI))
+int isalnum(int c) {
+  return (isalpha || isdigit);
+}
+
+
+__attribute__((STUB_ABI))
+int isspace(int c) {
+  return (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ');
+}
+
+
+__attribute__((STUB_ABI))
+long strtol(const char* nptr, char** endptr, register int base) {
+  register const char* s = nptr;
+  register unsigned long acc;
+  register int c;
+  register unsigned long cutoff;
+  register int neg = 0, any, cutlim;
+
+  /*
+   * Skip white space and pick up leading +/- sign if any.
+   * If base is 0, allow 0x for hex and 0 for octal, else
+   * assume decimal; if base is already 16, allow 0x.
+   */
+  do {
+    c = *s++;
+  } while (isspace(c));
+
+  if (c == '-') {
+    neg = 1;
+    c = *s++;
+  }
+  else if (c == '+') {
+    c = *s++;
+  }
+
+  if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X')) {
+    c = s[1];
+    s += 2;
+    base = 16;
+  }
+
+  if (base == 0) {
+    base = c == '0' ? 8 : 10;
+  }
+
+  /*
+   * Compute the cutoff value between legal numbers and illegal
+   * numbers.  That is the largest legal value, divided by the
+   * base.  An input number that is greater than this value, if
+   * followed by a legal input character, is too big.  One that
+   * is equal to this value may be valid or not; the limit
+   * between valid and invalid numbers is then based on the last
+   * digit.  For instance, if the range for longs is
+   * [-2147483648..2147483647] and the input base is 10,
+   * cutoff will be set to 214748364 and cutlim to either
+   * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+   * a value > 214748364, or equal but the next digit is > 7 (or 8),
+   * the number is too big, and we will return a range error.
+   *
+   * Set any if any `digits' consumed; make it negative to indicate
+   * overflow.
+   */
+  cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
+  cutlim = cutoff % (unsigned long)base;
+  cutoff /= (unsigned long)base;
+  for (acc = 0, any = 0;; c = *s++) {
+    if (isdigit(c))
+      c -= '0';
+    else if (isalpha(c))
+      c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+    else
+      break;
+    if (c >= base)
+      break;
+    if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+      any = -1;
+    else {
+      any = 1;
+      acc *= base;
+      acc += c;
+    }
+  }
+
+  if (any < 0) {
+    acc = neg ? LONG_MIN : LONG_MAX;
+  }
+  else if (neg) {
+    acc = -acc;
+  }
+
+  if (endptr != 0)
+    *endptr = (char *) (any ? s - 1 : nptr);
+
+  return (acc);
+}
+
+__attribute__((STUB_ABI))
+long long strtoll(const char* nptr, char** endptr, register int base) {
+  register const char* s = nptr;
+  register unsigned long long acc;
+  register int c;
+  register unsigned long long cutoff;
+  register int neg = 0, any, cutlim;
+
+  /*
+   * Skip white space and pick up leading +/- sign if any.
+   * If base is 0, allow 0x for hex and 0 for octal, else
+   * assume decimal; if base is already 16, allow 0x.
+   */
+  do {
+    c = *s++;
+  } while (isspace(c));
+
+  if (c == '-') {
+    neg = 1;
+    c = *s++;
+  }
+  else if (c == '+') {
+    c = *s++;
+  }
+
+  if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X')) {
+    c = s[1];
+    s += 2;
+    base = 16;
+  }
+
+  if (base == 0) {
+    base = c == '0' ? 8 : 10;
+  }
+
+  /*
+   * Compute the cutoff value between legal numbers and illegal
+   * numbers.  That is the largest legal value, divided by the
+   * base.  An input number that is greater than this value, if
+   * followed by a legal input character, is too big.  One that
+   * is equal to this value may be valid or not; the limit
+   * between valid and invalid numbers is then based on the last
+   * digit.  For instance, if the range for longs is
+   * [-2147483648..2147483647] and the input base is 10,
+   * cutoff will be set to 214748364 and cutlim to either
+   * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+   * a value > 214748364, or equal but the next digit is > 7 (or 8),
+   * the number is too big, and we will return a range error.
+   *
+   * Set any if any `digits' consumed; make it negative to indicate
+   * overflow.
+   */
+  cutoff = neg ? -(unsigned long long)LLONG_MIN : LLONG_MAX;
+  cutlim = cutoff % (unsigned long long)base;
+  cutoff /= (unsigned long long)base;
+  for (acc = 0, any = 0;; c = *s++) {
+    if (isdigit(c))
+      c -= '0';
+    else if (isalpha(c))
+      c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+    else
+      break;
+    if (c >= base)
+      break;
+    if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+      any = -1;
+    else {
+      any = 1;
+      acc *= base;
+      acc += c;
+    }
+  }
+
+  if (any < 0) {
+    acc = neg ? LLONG_MIN : LLONG_MAX;
+  }
+  else if (neg) {
+    acc = -acc;
+  }
+
+  if (endptr != 0) {
+    *endptr = (char *) (any ? s - 1 : nptr);
+  }
+
+  return (acc);
+}
+
+
+__attribute__((STUB_ABI))
+unsigned long strtoul(const char* nptr, char** endptr, register int base) {
+  register const char* s = nptr;
+  register unsigned long acc;
+  register int c;
+  register unsigned long cutoff;
+  register int neg = 0, any, cutlim;
+
+  /*
+   * See strtol for comments as to the logic used.
+   */
+  do {
+    c = *s++;
+  } while (isspace(c));
+
+  if (c == '-') {
+    neg = 1;
+    c = *s++;
+  }
+  else if (c == '+') {
+    c = *s++;
+  }
+
+  if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X')) {
+    c = s[1];
+    s += 2;
+    base = 16;
+  }
+
+  if (base == 0) {
+    base = c == '0' ? 8 : 10;
+  }
+
+  cutoff = (unsigned long)ULONG_MAX / (unsigned long)base;
+  cutlim = (unsigned long)ULONG_MAX % (unsigned long)base;
+  for (acc = 0, any = 0;; c = *s++) {
+    if (isdigit(c))
+      c -= '0';
+    else if (isalpha(c))
+      c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+    else
+      break;
+    if (c >= base)
+      break;
+    if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+      any = -1;
+    else {
+      any = 1;
+      acc *= base;
+      acc += c;
+    }
+  }
+
+  if (any < 0) {
+    acc = ULONG_MAX;
+  }
+  else if (neg) {
+    acc = -acc;
+  }
+
+  if (endptr != 0) {
+    *endptr = (char *) (any ? s - 1 : nptr);
+  }
+
+  return (acc);
+}
+
+
+__attribute__((STUB_ABI))
+unsigned long long strtoull(const char* nptr, char** endptr, register int base) {
+  register const char* s = nptr;
+  register unsigned long long acc;
+  register int c;
+  register unsigned long long cutoff;
+  register int neg = 0, any, cutlim;
+
+  /*
+   * See strtol for comments as to the logic used.
+   */
+  do {
+    c = *s++;
+  } while (isspace(c));
+
+  if (c == '-') {
+    neg = 1;
+    c = *s++;
+  }
+  else if (c == '+') {
+    c = *s++;
+  }
+
+  if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X')) {
+    c = s[1];
+    s += 2;
+    base = 16;
+  }
+
+  if (base == 0) {
+    base = c == '0' ? 8 : 10;
+  }
+
+  cutoff = (unsigned long long)ULLONG_MAX / (unsigned long long)base;
+  cutlim = (unsigned long long)ULLONG_MAX % (unsigned long long)base;
+  for (acc = 0, any = 0;; c = *s++) {
+    if (isdigit(c))
+      c -= '0';
+    else if (isalpha(c))
+      c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+    else
+      break;
+    if (c >= base)
+      break;
+    if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
+      any = -1;
+    else {
+      any = 1;
+      acc *= base;
+      acc += c;
+    }
+  }
+
+  if (any < 0) {
+    acc = ULLONG_MAX;
+  }
+  else if (neg) {
+    acc = -acc;
+  }
+
+  if (endptr != 0) {
+    *endptr = (char *) (any ? s - 1 : nptr);
+  }
+
+  return (acc);
+}
+
+
+__attribute__((STUB_ABI))
+int ffs (register int valu) {
+  register int bit;
+
+  if (valu == 0)
+    return 0;
+
+  for (bit = 1; !(valu & 1); bit++)
+    valu >>= 1;
+
+  return bit;
 }
