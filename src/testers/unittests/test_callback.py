fa4bbf99b70fb91d98fc5036b511c598fcc0e4ae
Christian Heitman <cheitman@quarkslab.com>
2022-09-14 16:37:18 -0300

    Fix call semantics when using GET_CONCRETE_MEMORY_VALUE callback (X86_86)

--- src/testers/unittests/test_callback.py
+++ src/testers/unittests/test_callback.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from triton import (TritonContext, ARCH, CALLBACK, Instruction)
+from triton import (TritonContext, ARCH, CALLBACK, Instruction, MemoryAccess)
 
 
 class TestCallback(unittest.TestCase):
@@ -85,3 +85,27 @@ class TestCallback(unittest.TestCase):
         self.Triton.removeCallback(CALLBACK.GET_CONCRETE_MEMORY_VALUE, self.method_callback)
         cb_new_refcnt = tuple(sys.getrefcount(x) for x in (cb, cb.__self__, cb.__func__))
         self.assertTrue(cb_initial_refcnt == cb_new_refcnt)
+
+    def test_call_rsp_issue(self):
+        def test_call(add_callback):
+            def get_concrete_memory_value_hook(ctx, mem):
+                pass
+
+            ctx = TritonContext(ARCH.X86_64)
+
+            rsp = 0xeffffd80
+
+            ctx.setConcreteMemoryValue(MemoryAccess(rsp + 0x48, 8), 0x41c8e0)
+            ctx.setConcreteMemoryValue(MemoryAccess(rsp + 0x50, 8), 0x41c910)
+
+            ctx.setConcreteRegisterValue(ctx.registers.rsp, rsp)
+
+            if add_callback:
+                ctx.addCallback(CALLBACK.GET_CONCRETE_MEMORY_VALUE, get_concrete_memory_value_hook)
+
+            ctx.processing(Instruction(0x41C690, b"\xff\x54\x24\x50"))  # call qword ptr [rsp + 0x50]
+
+            self.assertTrue(ctx.getConcreteRegisterValue(ctx.registers.rip) == 0x41c910)
+
+        test_call(False)
+        test_call(True)
