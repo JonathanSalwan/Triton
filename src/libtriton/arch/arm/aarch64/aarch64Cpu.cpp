33c15fd03279a986643a73e9c6cf2a1848701253
PsiFunction <shalaginmx998@yandex.ru>
2024-09-02 17:22:30 +0300

    + initial support for fp operand

--- src/libtriton/arch/arm/aarch64/aarch64Cpu.cpp
+++ src/libtriton/arch/arm/aarch64/aarch64Cpu.cpp
@@ -523,8 +523,72 @@ namespace triton {
                   break;
                 }
 
+                case triton::extlibs::capstone::ARM64_OP_FP: {
+                  if (size == 0) {
+                    throw triton::exceptions::Disassembly("Aarch64Cpu::disassembly(): Cannot correctly decode FP operand");
+                  }
+
+                  auto encode_fp_imm = [size](double fp_value) {
+                      static_assert(sizeof(float) == sizeof(triton::uint32), "Unexpected float type size");
+                      static_assert(sizeof(double) == sizeof(triton::uint64), "Unexpected double type size");
+
+                      auto IEEE754_f32_to_f16 = [](float value) -> uint16_t {
+                        uint32_t f;
+                        std::memcpy(&f, &value, sizeof(uint32_t));
+                        uint16_t sign = (f >> 16) & 0x8000;
+                        int16_t exponent = ((f >> 23) & 0xff) - 127 + 15;
+                        uint16_t mantissa = (f >> 13) & 0x3ff;
+
+                        if (exponent <= 0) {
+                            if (exponent < -10) {
+                                return sign;
+                            }
+                            mantissa = (mantissa | 0x400) >> (1 - exponent);
+                            return sign | mantissa;
+                        } else if (exponent == 0xff - (127 - 15)) {
+                            if (mantissa) {
+                                return sign | 0x7fff;
+                            } else {
+                                return sign | 0x7c00;
+                            }
+                        } else if (exponent > 30) {
+                            return sign | 0x7c00;
+                        }
+                        return sign | (exponent << 10) | mantissa;
+                      };  
+
+                      if (size == sizeof(double)) {
+                        triton::uint64 result;
+                        std:memcpy(&result, &fp_value, sizeof(double));
+                        return result;
+                      }
+                      else if (size == sizeof(float)) {
+                        float converted = static_cast<float>(fp_value);
+                        triton::uint32 conv_repr;
+                        std::memcpy(&conv_repr, &converted, sizeof(float));
+                        // just zero extended value
+                        return static_cast<triton::uint64>(conv_repr); 
+                      }
+                      else if (size == 2) { // half-precision
+                        float value = static_cast<float>(fp_value);
+                        return static_cast<triton::uint64>(IEEE754_f32_to_f16(value));
+                      } 
+
+                      throw triton::exceptions::Disassembly("AArch64Cpu::disassembly(): Invalid operand.");
+                  };
+
+                  Immediate imm{encode_fp_imm(op->fp), size};
+
+                  /* Set Shift type and value */
+                  imm.setShiftType(this->capstoneShiftToTritonShift(op->shift.type));
+                  imm.setShiftValue(op->shift.value);
+
+                  inst.operands.push_back(triton::arch::OperandWrapper(imm));
+                  }
+                  break;
+
                 default:
-                  /* NOTE: FP, CIMM, and missing one are not supported yet. */
+                  /* NOTE: CIMM, and missing one are not supported yet. */
                   throw triton::exceptions::Disassembly("AArch64Cpu::disassembly(): Invalid operand.");
               } // switch
             } // for operand
